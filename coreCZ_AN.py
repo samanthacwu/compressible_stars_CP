removes g_eff, fixes VH? updates visc_flux sign.

coreCZ_AN.py
@@ -136,7 +136,7 @@ ln_T  = field.Field(dist=d, bases=(b.radial_basis,), dtype=dtype)
 T_NCC = field.Field(dist=d, bases=(b.radial_basis,), dtype=dtype)
 inv_T = field.Field(dist=d, bases=(b,), dtype=dtype) #only on RHS, multiplies other terms
 H_eff = field.Field(dist=d, bases=(b,), dtype=dtype)
-g_eff = field.Field(dist=d, bases=(b.radial_basis,), tensorsig=(c,), dtype=dtype)
+#g_eff = field.Field(dist=d, bases=(b.radial_basis,), tensorsig=(c,), dtype=dtype)
 
 
 if args['--mesa_file'] is not None:
@@ -151,11 +151,11 @@ if args['--mesa_file'] is not None:
         H_eff['g']     = f['H_eff'][()][:,:,r_slice]
         inv_T['g']     = f['inv_T'][()][:,:,r_slice]
         T_NCC['g']     = f['T'][()][:,:,r_slice]
-        g_eff['g']     = f['g_eff'][()][:,:,:,r_slice]
+#        g_eff['g']     = f['g_eff'][()][:,:,:,r_slice]
         ρ['g']         = np.exp(f['ln_ρ'][()][:,:,r_slice].reshape(r1.shape))
-        T['g']         = T_NCC['g']
+        T['g']         = f['T'][()][:,:,r_slice].reshape(r1.shape)
 
-        t_buoy = np.sqrt(1/f['g_eff'][()].max())
+        t_buoy = 1#np.sqrt(1/f['T'][()].max())
 
     grad_ln_ρ  = grad(ln_ρ).evaluate()
     grad_ln_T  = grad(ln_T).evaluate()
@@ -167,7 +167,7 @@ logger.info('buoyancy time is {}'.format(t_buoy))
 max_dt = 0.5*t_buoy
 t_end = float(args['--buoy_end_time'])*t_buoy
 
-for f in [u, s1, p, ln_ρ, ln_T, inv_T, H_eff, g_eff, ρ]:
+for f in [u, s1, p, ln_ρ, ln_T, inv_T, H_eff, ρ]:#g_eff, ρ]:
     f.require_scales(dealias)
 
 r_vec = field.Field(dist=d, bases=(b,), tensorsig=(c,), dtype=dtype)
@@ -189,8 +189,8 @@ momentum_viscous_terms = div(stress1) + dot(grad_ln_ρ, stress1) - (2/3)*(grad(d
 trace_stress = trace(stress1)
 trace_stress.store_last = True
 VH1 = trace(dot(stress1, stress1))
-VH2 = -(1/3)*(trace_stress*trace_stress)
-VH  = VH1 + VH2
+VH2 = (trace_stress*trace_stress)
+VH  = 0.5*(VH1 - (1/3)*VH2)
 
 #TODO: Viscous heating
 
@@ -212,8 +212,8 @@ problem.add_equation(eq_eval("ddt(s1) - (1/Pe)*(lap(s1) + dot(grad(s1), (grad_ln
 problem.add_equation(eq_eval("u_r_bc = 0"), condition="nθ != 0")
 problem.add_equation(eq_eval("u_perp_bc = 0"), condition="nθ != 0")
 problem.add_equation(eq_eval("tau_u = 0"), condition="nθ == 0")
-problem.add_equation(eq_eval("radComp(grad(s1)(r=1)) = 0"))
-#problem.add_equation(eq_eval("s1(r=1) = 0"))
+problem.add_equation(eq_eval("s1(r=1) = 0"))
+#problem.add_equation(eq_eval("radComp(grad(s1)(r=1)) = 0"))
 
 print("Problem built")
 
@@ -345,7 +345,7 @@ class AnelasticRPW(RadialProfileWriter):
         #Get fluxes for energy output
 #        self.tasks['enth_flux'][:] = radial_averager(ρ['g']*u['g'][2,:]*(p['g'] + T['g']*s1['g'])) #need to subtract a 0.5 u dot u if I use dot(u, stress1) in mometum
         self.tasks['enth_flux'][:] = radial_averager(ρ['g']*u['g'][2,:]*(p['g'])) #need to subtract a 0.5 u dot u if I use dot(u, stress1) in mometum
-        self.tasks['visc_flux'][:] = radial_averager(ρ['g']*(self.fields['u·σ'][2,:] - (2/3)*u['g'][2,:]*self.fields['div_u'])/Re)
+        self.tasks['visc_flux'][:] = radial_averager(-ρ['g']*(self.fields['u·σ'][2,:] - (2/3)*u['g'][2,:]*self.fields['div_u'])/Re)
         self.tasks['cond_flux'][:] = radial_averager(-ρ['g']*T['g']*self.fields['grad_s'][2]/Pe)
         self.tasks['KE_flux'][:]   = radial_averager(0.5*ρ['g']*u['g'][2,:]*self.fields['u·u'])
 