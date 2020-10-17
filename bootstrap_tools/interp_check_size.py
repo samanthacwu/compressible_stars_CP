df92584fbeba986d2593026e9edbffa0e2f9d83e
Evan H. Anders <evanhanders@gmail.com>
2020-10-17 14:08:28 -0700

    actually does what previous commit claimed to do

--- bootstrap_tools/interp_check_size.py
+++ bootstrap_tools/interp_check_size.py
@@ -16,11 +16,13 @@ Options:
     --N=<N>                    initial resolution
     --node_size=<n>            Size of node; use to reduce memory constraints for large files
 """
+import pathlib
 from fractions import Fraction
 import time
 from collections import OrderedDict
 
 import numpy as np
+import h5py
 from docopt import docopt
 from mpi4py import MPI
 
@@ -29,6 +31,7 @@ from dedalus.tools import logging
 from dedalus.tools.parsing import split_equation
 from dedalus.extras.flow_tools import GlobalArrayReducer
 from dedalus.tools.config import config
+from dedalus.tools.general import natural_sort
 config['linear algebra']['MATRIX_FACTORIZER'] = 'SuperLUNaturalFactorizedTranspose'
 import dedalus_sphere
 
@@ -39,6 +42,126 @@ import logging
 logger = logging.getLogger(__name__)
 matplotlib_logger = logging.getLogger('matplotlib')
 matplotlib_logger.setLevel(logging.WARNING) 
+
+
+
+
+def merge_distributed_set(set_path, cleanup=False):
+    """
+    Merge a distributed analysis set from a FileHandler.
+
+    Parameters
+    ----------
+    set_path : str of pathlib.Path
+        Path to distributed analysis set folder
+    cleanup : bool, optional
+        Delete distributed files after merging (default: False)
+
+    """
+    set_path = pathlib.Path(set_path)
+    logger.info("Merging set {}".format(set_path))
+
+    set_stem = set_path.stem
+    proc_paths = set_path.glob("{}_p*.h5".format(set_stem))
+    proc_paths = natural_sort(proc_paths)
+    joint_path = set_path.parent.joinpath("{}.h5".format(set_stem))
+
+    # Create joint file, overwriting if it already exists
+    with h5py.File(str(joint_path), mode='w') as joint_file:
+        # Setup joint file based on first process file (arbitrary)
+        merge_setup(joint_file, proc_paths[0])
+        # Merge data from all process files
+        for proc_path in proc_paths:
+            merge_data(joint_file, proc_path)
+    # Cleanup after completed merge, if directed
+    if cleanup:
+        for proc_path in proc_paths:
+            proc_path.unlink()
+        set_path.rmdir()
+
+
+def merge_setup(joint_file, proc_path):
+    """
+    Merge HDF5 setup from part of a distributed analysis set into a joint file.
+
+    Parameters
+    ----------
+    joint_file : HDF5 file
+        Joint file
+    proc_path : str or pathlib.Path
+        Path to part of a distributed analysis set
+
+    """
+    proc_path = pathlib.Path(proc_path)
+    logger.info("Merging setup from {}".format(proc_path))
+
+    with h5py.File(str(proc_path), mode='r') as proc_file:
+        # File metadata
+        try:
+            joint_file.attrs['set_number'] = proc_file.attrs['set_number']
+        except KeyError:
+            joint_file.attrs['set_number'] = proc_file.attrs['file_number']
+        joint_file.attrs['handler_name'] = proc_file.attrs['handler_name']
+        try:
+            joint_file.attrs['writes'] = writes = proc_file.attrs['writes']
+        except KeyError:
+            joint_file.attrs['writes'] = writes = len(proc_file['scales']['write_number'])
+        # Copy scales (distributed files all have global scales)
+        proc_file.copy('scales', joint_file)
+        # Tasks
+        joint_tasks = joint_file.create_group('tasks')
+        proc_tasks = proc_file['tasks']
+        for taskname in proc_tasks:
+            # Setup dataset with automatic chunking
+            proc_dset = proc_tasks[taskname]
+            spatial_shape = proc_dset.attrs['global_shape']
+            joint_shape = (writes,) + tuple(spatial_shape)
+            joint_dset = joint_tasks.create_dataset(name=proc_dset.name,
+                                                    shape=joint_shape,
+                                                    dtype=proc_dset.dtype,
+                                                    chunks=True)
+            # Dataset metadata
+            joint_dset.attrs['task_number'] = proc_dset.attrs['task_number']
+            joint_dset.attrs['constant'] = proc_dset.attrs['constant']
+            joint_dset.attrs['grid_space'] = proc_dset.attrs['grid_space']
+            joint_dset.attrs['scales'] = proc_dset.attrs['scales']
+            # Dimension scales
+            for i, proc_dim in enumerate(proc_dset.dims):
+                joint_dset.dims[i].label = proc_dim.label
+#                for scalename in proc_dim:
+#                    scale = joint_file['scales'][scalename]
+#                    joint_dset.dims.create_scale(scale, scalename)
+#                    joint_dset.dims[i].attach_scale(scale)
+
+
+def merge_data(joint_file, proc_path):
+    """
+    Merge data from part of a distributed analysis set into a joint file.
+
+    Parameters
+    ----------
+    joint_file : HDF5 file
+        Joint file
+    proc_path : str or pathlib.Path
+        Path to part of a distributed analysis set
+
+    """
+    proc_path = pathlib.Path(proc_path)
+    logger.info("Merging data from {}".format(proc_path))
+
+    with h5py.File(str(proc_path), mode='r') as proc_file:
+        for taskname in proc_file['tasks']:
+            joint_dset = joint_file['tasks'][taskname]
+            proc_dset = proc_file['tasks'][taskname]
+            # Merge across spatial distribution
+            start = proc_dset.attrs['start']
+            count = proc_dset.attrs['count']
+            spatial_slices = tuple(slice(s, s+c) for (s,c) in zip(start, count))
+            # Merge maintains same set of writes
+            slices = (slice(None),) + spatial_slices
+            joint_dset[slices] = proc_dset[:]
+
+
 
 args   = docopt(__doc__)
 root_dir = args['<root_dir>']
@@ -87,9 +210,8 @@ if args['--start_check_folder'] is None:
     check_folder = '{:s}/final_checkpoint/final_checkpoint_s1/'.format(root_dir)
 else:
     check_folder = '{:s}'.format(args['--start_check_folder'])
-from dedalus.tools import post
 logger.info('merging old checkpoint')
-post.merge_distributed_set(check_folder)
+merge_distributed_set(check_folder)
 check_file = check_folder[:-1] + '.h5'
 
 u1 = field.Field(dist=d, bases=(b1,), tensorsig=(c,), dtype=dtype)
@@ -100,6 +222,7 @@ s2 = field.Field(dist=d, bases=(b2,), dtype=dtype)
 import h5py
 
 with h5py.File(check_file, 'r') as f:
+    [print(k) for k in f.keys()]
     if L_frac >= 1:
         u2['c'][:, :(Lmax+2), :Lmax+1, :Nmax+1] = f['tasks']['u'][-1,:]
         s2['c'][:(Lmax+2), :Lmax+1, :Nmax+1] = f['tasks']['s1'][-1,:]
