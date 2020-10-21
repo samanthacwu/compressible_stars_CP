fixes error where grad_s0 was changing signs near top of cz and making a thing, strong superadiabatic layer.

mesa_stars/make_halfStar_nccs.py
@@ -81,6 +81,7 @@ nablaT = p.gradT[::-1] #dlnT/dlnP
 T = 10**p.logT[::-1] * u.K
 cp = p.cp[::-1]  * u.erg / u.K / u.g
 cv = p.cv[::-1]  * u.erg / u.K / u.g
+opacity = p.opacity[::-1] * (u.cm**2 / u.g)
 mu = p.mu[::-1]
 N2 = p.brunt_N2[::-1] / u.s**2
 N2_structure   = p.brunt_N2_structure_term[::-1] / u.s**2
@@ -89,6 +90,10 @@ Luminosity = p.luminosity[::-1] * u.L_sun
 Luminosity = Luminosity.to('erg/s')
 L_conv = p.conv_L_div_L[::-1]*Luminosity
 csound = p.csound[::-1] * u.cm / u.s
+
+rad_diff = 16 * constants.sigma_sb.cgs * T**3 / (3 * rho**2 * cp * opacity)
+rad_diff = rad_diff.cgs
+
 
 cgs_G = constants.G.to('cm^3/(g*s^2)')
 g = cgs_G*mass/r**2
@@ -105,6 +110,8 @@ dlogrhodr = dlogPdr*(chiT/chiRho)*(nablaT_ad - nablaT) - g/csound**2
 dlogTdr   = dlogPdr*(nablaT)
 N2_therm_approx = g*(dlogPdr/gamma1 - dlogrhodr)
 grad_s = cp*N2/g #includes composition terms
+
+
 
 # Heating
 #H = rho * eps
@@ -145,8 +152,8 @@ bound_ind = np.argmin(np.abs(mass - core_cz_bound))
 
 
 #Nondimensionalization
-L = halfStar_r = r[-1]/2
-L_CZ  = r[bound_ind]
+halfStar_r = r[-1]/2
+L = L_CZ  = r[bound_ind]
 g0 = g[bound_ind] 
 rho0 = rho[0]
 P0 = P[0]
@@ -158,11 +165,36 @@ tau = tau.cgs
 print('one time unit is {:.2e}'.format(tau))
 u_H = L/tau
 
+Pe = u_H*L/rad_diff
+
+fig = plt.figure()
+ax1 = fig.add_subplot(2,1,1)
+plt.plot(r, N2, label='brunt2_full', c='k', lw=3)
+plt.plot(r, N2_structure, label='brunt2_structure', c='r')
+plt.plot(r, N2_composition, label='brunt2_comp', c='orange')
+plt.yscale('log')
+plt.ylabel(r'$N^2$')
+plt.legend(loc='best')
+ax2 = fig.add_subplot(2,1,2)
+plt.yscale('log')
+plt.axhline(1e3, c='k', lw=0.5)
+plt.axhline(1e4, c='k', lw=0.5)
+plt.axhline(1e5, c='k', lw=0.5)
+plt.plot(r, Pe, c='k', lw=3)
+plt.suptitle(filename)
+plt.ylabel(r'Pe')
+plt.xlabel(r'radius (cm)')
+plt.savefig('{:s}/{:s}_brunt_and_Pe_fig.png'.format(out_dir, filename.split('.data')[0]), dpi=200, bbox_inches='tight')
+
+
+
 Ma2 = u_H**2 / ((gamma0-1)*cp0*T0)
 s_c = Ma2*(gamma0-1)*cp0
 
+#maxR = float(1.1)
+#sim_bool = r <= 1.1*L#1.05*halfStar_r
 maxR = float(halfStar_r/L)
-sim_bool = r <= 1.05*halfStar_r
+sim_bool = r <= halfStar_r
 
 r_sim = r[sim_bool]/L
 
@@ -223,16 +255,6 @@ plot_ncc_figure(rg.flatten(), T_interp.flatten(), T_field['g'].flatten(), N, yla
 
 
 
-### effective heating / (rho * T)
-N = Nmax-1
-H_field = field.Field(dist=d, bases=(b,), dtype=np.float64)
-H_interp = np.interp(rg, r_sim, H_NCC[sim_bool])
-H_interp_plot = np.interp(rg, r_sim, (H_NCC * rho0*T0/rho/T)[sim_bool])
-H_field['g'] = H_interp / T_field['g'] / np.exp(ln_rho_field['g'])
-H_field['c'][:, :, N:] = 0
-plot_ncc_figure(rg.flatten(), H_interp_plot.flatten(), H_field['g'].flatten(), N, ylabel=r"$(H_{eff}/(\rho c_p T))$ (nondimensional)", fig_name="H_eff", out_dir=out_dir, zero_line=True)
-
-
 from scipy.special import erf
 def one_to_zero(x, x0, width=0.1):
     return (1 - erf( (x - x0)/width))/2
@@ -240,33 +262,54 @@ def one_to_zero(x, x0, width=0.1):
 def zero_to_one(*args, **kwargs):
     return -(one_to_zero(*args, **kwargs) - 1)
 
-if Nmax == 255:
+if Nmax == 127:
+    width = 0.03
+    N = 32
+    N_after = -1
+    center =  0.99*(L_CZ/L).value
+elif Nmax == 255:
     width = 0.02
     N = 127
-elif Nmax == 127:
-    width = 0.04
-    N = 63
+    N_after = -1
+    center = 0.99*(L_CZ/L).value
+elif Nmax == 383:
+    width = 0.015
+    N = 192
+    N_after = -1
+    center = 0.99*(L_CZ/L).value
 elif Nmax == 511:
-    width = 0.008
-    N = 127
-print(Nmax, width, N)
+    width = 0.01
+    N = 192
+    N_after = -1
+    center = 0.99*(L_CZ/L).value
+
+### effective heating / (rho * T)
+H_field = field.Field(dist=d, bases=(b,), dtype=np.float64)
+H_interp = np.interp(rg, r_sim, H_NCC[sim_bool])
+H_interp_plot = np.interp(rg, r_sim, (H_NCC * rho0*T0/rho/T)[sim_bool])
+H_field['g'] = H_interp / T_field['g'] / np.exp(ln_rho_field['g'])
+plot_ncc_figure(rg.flatten(), H_interp_plot.flatten(), H_field['g'].flatten(), N, ylabel=r"$(H_{eff}/(\rho c_p T))$ (nondimensional)", fig_name="H_eff", out_dir=out_dir, zero_line=True)
+
+### entropy gradient
 grad_s_field  = field.Field(dist=d, bases=(b,), dtype=np.float64, tensorsig=(c,))
 grad_s_interp = np.interp(rg, r_sim, grad_s[sim_bool]*L/s_c)
-grad_s_field['g'][2] = grad_s_interp
+grad_s_base = np.copy(grad_s_interp)
+flat_window = (rg.flatten() > 0.95)*(rg.flatten() < 1.015)
+arg_flat     = np.argmin(np.abs(grad_s_interp.flatten() - grad_s_interp.flatten()[flat_window].max()))
+grad_s_base[:,:,:arg_flat] = grad_s_interp[:,:,arg_flat]# grad_s_interp.flatten() > 0].flatten()[0]
+#grad_s_base[:,:,grad_s_interp.flatten() <= 0] = grad_s_interp[:,:, grad_s_interp.flatten() > 0].flatten()[0]
+grad_s_field['g'][2] = grad_s_base
 grad_s_field['c'][:,:,:,N:] = 0
-grad_s_field['g'][2] *= zero_to_one(rg, 0.995*(L_CZ/L).value, width=width*(L_CZ/L).value)
-grad_s_field['c'][:,:,:,-1] = 0
+grad_s_field['g'][2] *= zero_to_one(rg, center, width=width*(L_CZ/L).value)
+grad_s_field['c'][:,:,:,N_after:] = 0
 plot_ncc_figure(rg.flatten(), grad_s_interp.flatten(), grad_s_field['g'][2].flatten(), N, ylabel=r"$L(\nabla s/s_c)$", fig_name="grad_s", out_dir=out_dir, zero_line=True)
-
-print(np.abs(grad_s_field['g'][0]).max())
-print(np.abs(grad_s_field['g'][1]).max())
-
 
 
 plt.figure()
-plt.plot(rg.flatten(), np.abs(grad_s_field['g'][2].flatten()))
-#plt.plot(rg.flatten(), -grad_s_field['g'][2].flatten(), ls='--')
+plt.plot(rg.flatten(), grad_s_field['g'][2].flatten())
+plt.plot(rg.flatten(), -grad_s_field['g'][2].flatten(), ls='--')
 plt.plot(rg.flatten(), grad_s_interp.flatten()) 
+plt.plot(rg.flatten(), grad_s_base.flatten()) 
 plt.yscale('log')
 plt.show()
 