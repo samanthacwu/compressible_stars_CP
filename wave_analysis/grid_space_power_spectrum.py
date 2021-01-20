218255975a2d8daa9a9b55f0e7490cc49165bec2
Evan H. Anders <evanhanders@gmail.com>
2021-01-20 15:45:04 -0800

    power spectrum operations take a bit less memory

--- wave_analysis/grid_space_power_spectrum.py
+++ wave_analysis/grid_space_power_spectrum.py
@@ -8,7 +8,7 @@ Usage:
     plot_mollweide_snapshots.py <root_dir> [options]
 
 Options:
-    --data_dir=<dir>                    Name of data handler directory [default: SH_transform_surface_shell_slices]
+    --data_dir=<dir>                    Name of data handler directory [default: surface_shell_slices]
     --start_fig=<fig_start_num>         Number of first figure file [default: 1]
     --start_file=<file_start_num>       Number of Dedalus output file to start plotting at [default: 1]
     --n_files=<num_files>               Total number of files to plot
@@ -73,9 +73,28 @@ with h5py.File(star_file, 'r') as f:
     N2plateau = f['N2plateau'][()] * (60*60*24)**2
 
 # Create Plotter object, tell it which fields to plot
-out_dir = 'power_spectra'.format(data_dir)
+out_dir = 'grid_power_spectra'.format(data_dir)
 full_out_dir = '{}/{}'.format(root_dir, out_dir)
 if not args['--plot_only']:
+    # Parameters
+    dtype = np.float64
+    Lmax = int(root_dir.split('Re')[-1].split('_')[1].split('x')[0])
+
+    # Bases
+    dealias = 1
+    c = coords.S2Coordinates('φ', 'θ')
+    d = distributor.Distributor((c,), mesh=None, comm=MPI.COMM_SELF)
+    b = basis.SWSH(c, (2*(Lmax+2), Lmax+1), radius=float(args['--radius']), dtype=dtype)
+    φ, θ = b.local_grids((dealias, dealias))
+    φg, θg = b.global_grids((dealias, dealias))
+
+    weight_φ = np.gradient(φg.flatten()).reshape(φg.shape)#(np.ones_like(φg)*np.pi/((b.Lmax+2)*dealias))
+    weight_θ = b.global_colatitude_weights(dealias)
+    weight = weight_θ * weight_φ
+    volume = np.sum(weight)
+
+    field = field.Field(dist=d, bases=(b,), dtype=dtype)
+
     plotter = SFP(root_dir, file_dir=data_dir, fig_name=out_dir, start_file=start_file, n_files=n_files, distribution='single')
     fields = ['s1_surf',]#, 'u_theta_surf',]
 
@@ -85,14 +104,11 @@ if not args['--plot_only']:
         print('reading file {}...'.format(plotter.current_filenum+1))
         file_name = plotter.files[plotter.current_filenum]
         with h5py.File('{}'.format(file_name), 'r') as f:
-            if plotter.current_filenum == 0:
-                ells = f['ells'][()]
-                ms = f['ms'][()]
-            times.append(f['time'][()])
+            times.append(f['scales/sim_time'][()])
         plotter.current_filenum += 1
 
     times = np.concatenate(times)
-    data_cube = np.zeros((times.shape[0], ells.shape[1], ms.shape[2]), dtype=np.complex128)
+    data_cube = np.zeros((times.shape[0], *tuple(field['g'].shape)), dtype=dtype)
 
     print('filling datacube...')
     writes = 0
@@ -100,57 +116,42 @@ if not args['--plot_only']:
         print('reading file {}...'.format(plotter.current_filenum+1))
         file_name = plotter.files[plotter.current_filenum]
         with h5py.File('{}'.format(file_name), 'r') as f:
-            this_file_writes = len(f['time'][()])
-            data_cube[writes:writes+this_file_writes,:] = f['s1_surf'][:,:,:]
+            this_file_writes = len(f['scales/sim_time'][()].flatten())
+            data_cube[writes:writes+this_file_writes,:] = f['tasks/s1_surf'][()].squeeze()
             writes += this_file_writes
         plotter.current_filenum += 1
-
-    #Get back to proper grid units.
-    data_cube[:,:,0]  /= np.sqrt(2) # m == 0
-    data_cube[:,:,1:] /= np.sqrt(4) # m != 0
 
     print('taking transform')
     dt = np.mean(np.gradient(times))
     freqs = np.fft.fftfreq(times.shape[0], d=dt)
     window = np.hanning(times.shape[0]).reshape((times.shape[0], 1))
-#    transform = np.fft.fft(data_cube, axis=0)
-    transform = np.zeros(data_cube.shape, dtype=np.complex128)
+    transform = np.zeros(tuple(data_cube.shape), dtype=np.complex128)
     for i in range(data_cube.shape[2]):
         print('taking transform {}/{}'.format(i+1, data_cube.shape[2]))
-        transform[:,:,i] = 4*np.pi*np.fft.fft(window*data_cube[:,:,i], axis=0)
+        transform[:,:,i] = np.fft.fft(window*data_cube[:,:,i], axis=0)
     power = transform*np.conj(transform) / (freqs.shape[0]/2)**2
-    power = np.sum(power, axis=2) #sum over m's
 
-    with h5py.File('{}/power_spectra.h5'.format(full_out_dir), 'w') as f:
+    #Sum over theta and phi, use weights & volume to get vol_avg, then scale by true vol (4pi)
+    power = 4*np.pi*np.sum(np.sum(weight*power, axis=2), axis=1)/volume
+
+    with h5py.File('{}/grid_power_spectra.h5'.format(full_out_dir), 'w') as f:
         f['power'] = power
-        f['ells']  = ells
         f['freqs'] = freqs 
         f['freqs_inv_day'] = freqs/tau
 else:  
-    with h5py.File('{}/power_spectra.h5'.format(full_out_dir), 'r') as f:
+    with h5py.File('{}/grid_power_spectra.h5'.format(full_out_dir), 'r') as f:
         power = f['power'][()]
-        ells = f['ells'][()]
         freqs = f['freqs'][()]
 
 freqs /= tau
 good = freqs >= 0
 min_freq = 1e-1
 max_freq = freqs.max()
-sum_power = np.zeros(power.shape[0])
-sum_power2 = np.zeros(power.shape[0])
-#sum_power2 = np.sum(power, axis=1)
-for i, ell in enumerate(ells.flatten()):
-    if ell == 0:
-        continue
-    sum_power += power[:,i].real/ell
-    if ell <= 10:
-        sum_power2 += power[:,i].real/ell
         
-ymin = sum_power[(freqs > 5e-2)*(freqs < max_freq)][-1].min()/2
-ymax = sum_power[(freqs > 5e-2)*(freqs <= max_freq)].max()*2
+ymin = power[(freqs > 5e-2)*(freqs < max_freq)][-1].min()/2
+ymax = power[(freqs > 5e-2)*(freqs <= max_freq)].max()*2
 
-plt.plot(freqs[good], sum_power2[good], c = 'green', ls='--', label=r'$\ell <= 10$')
-plt.plot(freqs[good], sum_power[good], c = 'k', label=r'all $\ell$ values')
+plt.plot(freqs[good], power[good], c = 'k')
 plt.yscale('log')
 plt.xscale('log')
 plt.ylabel(r'Power (simulation s1 units squared)')
@@ -160,43 +161,3 @@ plt.xlim(min_freq, max_freq)
 plt.ylim(ymin, ymax)
 plt.legend(loc='best')
 plt.savefig('{}/summed_power.png'.format(full_out_dir), dpi=600)
-
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-#small_sum = np.zeros_like(sum_power)
-#last_N = 20
-#for i, ell in enumerate(ells.flatten()):
-#    j = last_N - i
-#    this_ell = last_N - ell
-#    if this_ell == 0:
-#        break
-#    plt.plot(freqs[good], power[good,j].real/this_ell, label=r'$\ell={{{}}}$'.format(this_ell))
-#    small_sum += power[:,j].real/this_ell
-#    ax1.set_yscale('log')
-#    ax1.set_xscale('log')
-#    ax1.set_ylabel(r'Power$/\ell$ (simulation s1 units squared)')
-#    ax1.set_xlabel(r'Frequency (day$^{-1}$)')
-#    ax1.set_xlim(min_freq, max_freq)
-#    ax1.set_ylim(ymin, ymax)
-#ax1.plot(freqs[good], small_sum[good], c='k', label='sum')
-#plt.legend(loc='upper right')
-#fig.savefig('{}/power_ell0-{}.png'.format(full_out_dir, last_N), dpi=600)
-ax1.cla()
-for i, ell in enumerate(ells.flatten()):
-    if ell == 0:
-        continue
-    plt.plot(freqs[good], power[good,i].real/ell, c='k')
-    ax1.set_yscale('log')
-    ax1.set_xscale('log')
-    ax1.text(0.05, 0.95, r'$\ell = {{{}}}$'.format(ell), transform=ax1.transAxes)
-    ax1.set_ylabel(r'Power$/\ell$ (simulation s1 units squared)')
-    ax1.set_xlabel(r'Frequency (day$^{-1}$)')
-    ax1.set_xlim(min_freq, max_freq)
-    ax1.set_ylim(ymin, ymax)
-    fig.savefig('{}/power_ell{}.png'.format(full_out_dir, ell), dpi=600)
-    ax1.cla()
-    if ell > 10:
-        break
-
-    
-
