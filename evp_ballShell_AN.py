fixes inner product for dual bases & sets up transfer/wave-flux pipeline all in terms of sim time units.

evp_ballShell_AN.py
@@ -500,6 +500,18 @@ weight_rB1 = bB1.radial_basis.local_weights(dealias)
 weight_rS1 = bS1.radial_basis.local_weights(dealias)*rS1**2
 weight1 = weight_θ1 * weight_φ1
 volume1 = np.sum(weight1)
+weight1 *= 4*np.pi / volume1
+
+rB_volume1 = np.sum(weight1*weight_rB1)
+rS_volume1 = np.sum(weight1*weight_rS1)
+
+weight_rB1 *= ((4/3) * np.pi * r_inner**3) / rB_volume1
+weight_rS1 *= ((4/3) * np.pi * (r_outer**3 - r_inner**3)) / rS_volume1
+full_weight_1 = weight1 * np.concatenate((weight_rB1.flatten(), weight_rS1.flatten()))[None, None, :]
+
+print(volume1 / (4*np.pi))
+print(rB_volume1 / ((4/3) * np.pi * r_inner**3))
+print(rS_volume1 / ((4/3) * np.pi * (r_outer**3 - r_inner**3)))
 
 if NmaxB_hires is not None:
     bB2   = basis.BallBasis(c, (2*(Lmax+1), Lmax+1, NmaxB_hires+1), radius=r_inner, dtype=dtype)
@@ -533,12 +545,12 @@ angComp   = lambda A, index=1: operators.AngularComponent(A, index=index)
 
 r1 = np.concatenate((rB1.flatten(), rS1.flatten()))
 radial_weights_1 = np.concatenate((weight_rB1.flatten(), weight_rS1.flatten()), axis=-1)
-def IP(velocity1, velocity2):
+def IP(velocity1, velocity2, density):
     """ Integrate the bra-ket of two eigenfunctions of velocity. """
     int_field = np.sum(velocity1*np.conj(velocity2), axis=0)
-    return np.sum(int_field*radial_weights_1)/np.sum(radial_weights_1)
+    return np.sum(density*int_field*full_weight_1)
 
-def calculate_duals(velocity_list):
+def calculate_duals(velocity_list, density):
     """
     Calculate the dual basis of the velocity eigenvectors.
     """
@@ -548,7 +560,7 @@ def calculate_duals(velocity_list):
     for i in range(n_modes):
         if i % 10 == 0: logger.info("duals {}/{}".format(i, n_modes))
         for j in range(n_modes):
-            IP_matrix[i,j] = IP(velocity_list[i], velocity_list[j])
+            IP_matrix[i,j] = IP(velocity_list[i], velocity_list[j], density)
     
     print('dual IP matrix cond: {:.3e}'.format(np.linalg.cond(IP_matrix)))
     IP_inv = np.linalg.inv(IP_matrix)
@@ -620,7 +632,6 @@ for i in range(Lmax):
             if rv/L_mesa > 1.0 and rv/L_mesa < r_outer:
                 opt_depth += depth_integrand[i]
         depths.append(opt_depth)
-    print(depths)
 
         
 
@@ -628,6 +639,7 @@ for i in range(Lmax):
     bB = namespace1['bB']
     ρS = namespace1['ρS']
     ρB = namespace1['ρB']
+    ρ  = np.concatenate((ρB['g'][0,0,:], ρS['g'][0,0,:]))
     pS = namespace1['pS']
     pB = namespace1['pB']
     uS = namespace1['uS']
@@ -714,8 +726,10 @@ for i in range(Lmax):
 #        print(subsystem.group, s1_amplitudes[i], old_s1_amplitudes, integ_energies[i], old_integ_energy, s1_amplitudes[i]/old_s1_amplitudes, integ_energies[i]/old_integ_energy)
 
     with h5py.File('{:s}/ell{:03d}_eigenvalues.h5'.format(out_dir, ell), 'w') as f:
-        f['good_evalues'] = solver1.eigenvalues/tau
-        f['good_omegas']  = solver1.eigenvalues.real/tau
+        f['good_evalues'] = solver1.eigenvalues
+        f['good_omegas']  = solver1.eigenvalues.real
+        f['good_evalues_inv_day'] = solver1.eigenvalues/tau
+        f['good_omegas_inv_day']  = solver1.eigenvalues.real/tau
         f['s1_amplitudes']  = s1_amplitudes
         f['integ_energies'] = integ_energies
         f['wave_flux_eigenfunctions'] = np.array(wave_flux_eigenfunctions)
@@ -727,10 +741,12 @@ for i in range(Lmax):
         f['ρS'] = namespace1['ρS']['g']
         f['depths'] = np.array(depths)
 
-    velocity_duals = calculate_duals(velocity_eigenfunctions)
+    velocity_duals = calculate_duals(velocity_eigenfunctions, ρ[None, None, :])
     with h5py.File('{:s}/duals_ell{:03d}_eigenvalues.h5'.format(out_dir, ell), 'w') as f:
-        f['good_evalues'] = solver1.eigenvalues/tau
-        f['good_omegas']  = solver1.eigenvalues.real/tau
+        f['good_evalues'] = solver1.eigenvalues
+        f['good_omegas']  = solver1.eigenvalues.real
+        f['good_evalues_inv_day'] = solver1.eigenvalues/tau
+        f['good_omegas_inv_day']  = solver1.eigenvalues.real/tau
         f['s1_amplitudes']  = s1_amplitudes
         f['integ_energies'] = integ_energies
         f['wave_flux_eigenfunctions'] = np.array(wave_flux_eigenfunctions)

transfer_ballShell.py
@@ -21,13 +21,15 @@ args = docopt(__doc__)
 #TODO: fix rho
 if args['--mesa_file'] is not None:
     with h5py.File(args['--mesa_file'], 'r') as mf:
-        r_mesa = mf['r_mesa'][()]/mf['L'][()]
-        N2_mesa = mf['N2_mesa'][()]*((60*60*24)/(2*np.pi))**2
-        S1_mesa = mf['S1_mesa'][()]*(60*60*24)/(2*np.pi)
-        N2_mesa_full = mf['N2_mesa'][()]
-        g_mesa  = mf['g_mesa'][()]
-        cp_mesa = mf['cp_mesa'][()]
-        pre_PE = g_mesa**2/cp_mesa**2/N2_mesa_full * (mf['tau'][()]*mf['s_c'][()]/mf['L'][()])**2
+#        r_mesa = mf['r_mesa'][()]/mf['L'][()]
+#        N2_mesa = mf['N2_mesa'][()]*((60*60*24)/(2*np.pi))**2
+#        S1_mesa = mf['S1_mesa'][()]*(60*60*24)/(2*np.pi)
+#        N2_mesa_full = mf['N2_mesa'][()]
+#        g_mesa  = mf['g_mesa'][()]
+#        cp_mesa = mf['cp_mesa'][()]
+#        pre_PE = g_mesa**2/cp_mesa**2/N2_mesa_full * (mf['tau'][()]*mf['s_c'][()]/mf['L'][()])**2
+        tau_s = mf['tau'][()]
+        tau = tau_s/(60*60*24)
         rB = mf['rB']
         rS = mf['rS']
         ρB = np.exp(mf['ln_ρB'])
@@ -80,6 +82,7 @@ print(ell_list)
 dir = args['<root_dir>']
 
 for ell in ell_list:
+    plt.figure()
     print("ell = %i" % ell)
 
     transfers = []
@@ -88,6 +91,7 @@ for ell in ell_list:
         with h5py.File('{:s}/duals_ell{:03d}_eigenvalues.h5'.format(dir, ell), 'r') as f:
             velocity_duals = f['velocity_duals'][()]
             values = f['good_evalues'][()]
+            values_inv_day = f['good_evalues_inv_day'][()]
             velocity_eigenfunctions = f['velocity_eigenfunctions'][()]
             s1_amplitudes = f['s1_amplitudes'][()]
             depths = f['depths'][()]
@@ -102,8 +106,8 @@ for ell in ell_list:
         velocity_eigenfunctions = velocity_eigenfunctions[good]
         velocity_duals = velocity_duals[good]
      
-        om0 = values.real[-2]
-        om1 = values.real[0]*2
+        om0 = values.real[-1]
+        om1 = 1.1*values.real[0]
         om = np.exp( np.linspace(np.log(om0), np.log(om1), num=5000, endpoint=True) )
 
         r0 = 1.1
@@ -119,7 +123,7 @@ for ell in ell_list:
 
 
     #    plt.loglog(om, T)
-        plt.loglog(om/(2*np.pi), np.abs(T)**2*om**(-13/2), lw=3-j, label='depth filter = {}'.format(d_filter))
+        plt.loglog(om/(2*np.pi) / tau, np.abs(T)**2*om**(-13/2), lw=3-j, label='depth filter = {}'.format(d_filter))
         oms.append(om)
         transfers.append(T)
 
@@ -139,9 +143,12 @@ for ell in ell_list:
 
     with h5py.File('{:s}/transfer_ell{:03d}_eigenvalues.h5'.format(dir, ell), 'w') as f:
         f['om'] = good_om
+        f['om_inv_day'] = good_om / tau
         f['transfer'] = good_T
-    plt.loglog(good_om/(2*np.pi), np.abs(good_T)**2*good_om**(-13/2), lw=1, label='combined')
+    print('{:.3e}'.format((np.abs(good_T)**2*good_om**(-13/2)).max()))
+    plt.loglog(good_om/(2*np.pi) / tau, np.abs(good_T)**2*good_om**(-13/2), lw=1, label='combined')
     plt.xlabel('frequency 1/day')
     plt.legend()
-    plt.show()
+    plt.title("ell = %i" % ell)
+plt.show()
 

wave_analysis/compare_eigenvalues_and_frequencies.py
@@ -28,6 +28,7 @@ transfer_file = args['--transfer_file']
 ell = int(evp_data_file.split('ell')[-1].split('_')[0])
 with h5py.File(evp_data_file, 'r') as ef:
     complex_eigenvalues = ef['good_evalues'][()]
+    complex_eigenvalues_inv_day = ef['good_evalues_inv_day'][()]
     s1_amplitudes = ef['s1_amplitudes'][()]
     integ_energies = ef['integ_energies'][()]
 
@@ -37,23 +38,25 @@ with h5py.File(sh_spectrum_file, 'r') as sf:
     except:
         power_per_ell = sf['s1_power_per_ell'][()]
     ells = sf['ells'][()]
-    freqs = sf['freqs_inv_day'][()]
+    freqs = sf['freqs'][()]
+    freqs_inv_day = sf['freqs_inv_day'][()]
     power = power_per_ell[:,ells.flatten() == ell]
 
 if transfer_file is not None:
     with h5py.File(transfer_file, 'r') as f:
         transfer = f['transfer'][()]
         transfer_om = f['om'][()]
+        transfer_om_inv_day = f['om_inv_day'][()]
 else:
     transfer = transfer_om = None
         
 
 fig = plt.figure()
-for f in complex_eigenvalues:
+for f in complex_eigenvalues_inv_day:
     print(f.real)
     plt.axvline(np.abs(f.real)/(2*np.pi), c='b', lw=0.5)
 
-plt.loglog(freqs, power, c='k')
+plt.loglog(freqs_inv_day, power, c='k')
 plt.xlim(1e-1, 1e2)
 plt.xlabel('frequency (day$^{-1}$)')
 plt.ylabel('power')
@@ -61,7 +64,7 @@ plt.title(r'$\ell = ${}'.format(ell))
 plt.savefig('scratch/ell{:03d}_identified_eigenmodes.png'.format(ell), dpi=300)
 
 fig = plt.figure()
-for f in complex_eigenvalues:
+for f in complex_eigenvalues_inv_day:
     plt.scatter(np.abs(f.real)/(2*np.pi), np.abs(1/f.imag)*(2*np.pi), c='b')
 plt.yscale('log')
 plt.xscale('log')
@@ -123,61 +126,77 @@ if wave_flux_file is None:
 else:
     with h5py.File(args['--mesa_file'], 'r') as f:
         r_mesa = f['r_mesa'][()]/f['L'][()]
-        N2_mesa = f['N2_mesa'][()] * (60*60*24)**2 #1/s^2 -> 1/day^2
-        N2_inv_day_func = interp1d(r_mesa, N2_mesa)
+        N2_mesa = f['N2_mesa'][()]*f['tau'][()]**2
+        N2_mesa_func = interp1d(r_mesa, N2_mesa)
 
         sim_grad_T = np.concatenate( (f['grad_TB'][()][2,:].squeeze(), f['grad_TS'][()][2,:].squeeze()) )
         sim_grad_s0 = np.concatenate( (f['grad_s0B'][()][2,:].squeeze(), f['grad_s0S'][()][2,:].squeeze()) )
+        sim_grad_ln_ρ = np.concatenate( (f['grad_ln_ρB'][()][2,:].squeeze(), f['grad_ln_ρS'][()][2,:].squeeze()) )
         sim_ln_ρ = np.concatenate( (f['ln_ρB'][()].squeeze(), f['ln_ρS'][()].squeeze()) )
         sim_radius = np.concatenate( (f['rB'][()].squeeze(), f['rS'][()].squeeze()) )
 
         grad_s0_func = interp1d(sim_radius, sim_grad_s0)
         grad_T_func = interp1d(sim_radius, sim_grad_T)
+        grad_ln_ρ_func = interp1d(sim_radius, sim_grad_ln_ρ)
         ln_ρ_func = interp1d(sim_radius, sim_ln_ρ)
         N2_sim = lambda r: -grad_T_func(r)*grad_s0_func(r)
         
        
     radius = 1.15
-    k_h = np.sqrt(ell*(ell+1))/radius
 
     with h5py.File(wave_flux_file, 'r') as f:
         ells = np.expand_dims(f['ells'][()].flatten(), axis=0)
-        freqs_inv_day = np.expand_dims(f['real_freqs_inv_day'][()], axis=1)
-        freqs_sim = np.expand_dims(f['real_freqs'][()], axis=1)
-        d2F_dell_df = f['wave_flux'][()]  #wave flux calculation is currently r^2 * rho(r) * hat(ur) * conj(hat(p))
-        d2F_dlnell_dlnf = ells*freqs_sim*d2F_dell_df
-        sim_omegas = 2*np.pi*freqs_inv_day
+        waveflux_freqs_inv_day = np.expand_dims(f['real_freqs_inv_day'][()], axis=1)
+        waveflux_freqs = np.expand_dims(f['real_freqs'][()], axis=1)
+        d2F_dell_df = 4 * np.pi * f['wave_flux'][()]  #wave flux calculation is currently r^2 * rho(r) * hat(ur) * conj(hat(p))
+        d2F_dlnell_dlnf = ells*waveflux_freqs*d2F_dell_df
+        waveflux_omegas = 2*np.pi*waveflux_freqs
         this_ell_flux = d2F_dlnell_dlnf[:, ells.flatten() == ell]
-        tau = freqs_sim.max()/freqs_inv_day.max()
+        tau = waveflux_freqs.max()/waveflux_freqs_inv_day.max()
 
     #wave flux vs omega fit
+    #fit a omega^{freq_power} function to omega between the 3rd highest freq eigenvalue and the median eigenvalue
     nvals = len(complex_eigenvalues)
-    good = (sim_omegas.flatten() > complex_eigenvalues.real[int(nvals/2)])*(sim_omegas.flatten() < complex_eigenvalues.real[2])
-    x = np.log10(sim_omegas.flatten())
+    good = (waveflux_omegas.flatten() > complex_eigenvalues.real[int(nvals/2)])*(waveflux_omegas.flatten() < complex_eigenvalues.real[2])
+    x = np.log10(waveflux_omegas.flatten())
     y = np.log10(this_ell_flux.flatten()/(10**(x))**(float(Fraction(args['--freq_power']))))
     mean_offset = np.sum(np.gradient(x[good])*y[good])/np.sum(np.gradient(x[good]))
     wave_flux_func = lambda omega: 10**(mean_offset)*omega**(float(Fraction(args['--freq_power'])))
     print('wave flux: {:.3e} * omega ^ {}'.format(10**(mean_offset), args['--freq_power']))
 
-    #Shiode eqn 9
+#    plt.figure()
+#    plt.loglog(10**x, this_ell_flux.flatten())
+#    plt.loglog(10**x, wave_flux_func(10**x))
+#    plt.show()
+
+    #Shiode eqn a
     fudge_factor_shiode = 1
-    shiode_energies = fudge_factor_shiode * (0.5 * Nm*wave_flux_func(complex_eigenvalues.real)/np.abs(tau*complex_eigenvalues.imag) / radius**2)
+    shiode_energies = fudge_factor_shiode * (0.5 * Nm*wave_flux_func(complex_eigenvalues.real)/np.abs(complex_eigenvalues.imag) / (4*np.pi*radius**2))
     adjusted_energies = np.abs(s1_amplitudes**2/integ_energies) * shiode_energies
 
 
     #Transfer function
-    sim_k_r = np.sqrt((N2_sim(radius)/(tau)**2/transfer_om**2 - 1)*k_h**2)
-    sim_k = np.sqrt(sim_k_r**2 + k_h**2)
-    u_r = np.sqrt( ( 2*np.pi*sim_k**2/(ell*sim_k_r) ) * (1/(radius**2 * np.exp(ln_ρ_func(radius))*N2_sim(radius))) * wave_flux_func(transfer_om ) )
+    H = -1/grad_ln_ρ_func(radius)
+    k_h = np.sqrt(ell*(ell+1))/radius
+#    k_r = np.sqrt((N2_mesa_func(radius)/transfer_om**2 - 1)*k_h**2 - 1/(4*H**2))
+    k_r = np.sqrt((N2_sim(radius)/transfer_om**2 - 1)*k_h**2 - 1/(4*H**2))
+    k = np.sqrt(k_r**2 + k_h**2)
+
+    print(transfer_om.max(), transfer_om_inv_day.max())
+    u_r2 = wave_flux_func(transfer_om)
+    u_r2 *= transfer_om * k**2 / k_r
+    u_r2 /= 4*np.pi*radius**2 * np.exp(ln_ρ_func(1)) * N2_sim(1)
+#    u_r = np.sqrt( ( (transfer_om_sim)*k**2/(k_r) ) * (1/((4*np.pi*radius**2) * np.exp(ln_ρ_func(radius))*N2_sim(radius))) * wave_flux_func(transfer_om) )
+    u_r = np.sqrt(u_r2)
     fudge_factor_transfer = 1
     transfer_power = fudge_factor_transfer*(transfer*u_r)**2
 
 
 fig = plt.figure()
-plt.loglog(freqs, power, c='k', label='IVP')
-plt.scatter(real_frequencies, adjusted_energies, c='blue', label='shiode')
+plt.loglog(freqs_inv_day, power, c='k', label='IVP')
+plt.scatter(complex_eigenvalues_inv_day.real/(2*np.pi), adjusted_energies, c='blue', label='shiode')
 if transfer_file is not None:
-    plt.loglog(transfer_om/(2*np.pi), transfer_power, c='orange', alpha=0.8, label='transfer')
+    plt.loglog(transfer_om_inv_day/(2*np.pi), transfer_power, c='orange', alpha=0.8, label='transfer')
 plt.xlim(1e-1, 1e1)
 plt.xlabel('frequency (day$^{-1}$)')
 plt.ylabel('power')
@@ -197,3 +216,6 @@ plt.xlabel('frequency (day$^{-1}$)')
 plt.ylabel('|s1|$^2$ / integ energy')
 plt.title(r'$\ell = ${}'.format(ell))
 plt.savefig('scratch/ell{:03d}_energy_s1_ratio.png'.format(ell), dpi=300)
+
+
+