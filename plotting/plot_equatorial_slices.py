4515cf34b7aa20862241f5676fe95f00e8304b50
Evan H. Anders <evanhanders@gmail.com>
2021-09-08 15:24:31 -0700

    new plotpal interface for equatorial slices.

--- plotting/plot_equatorial_slices.py
+++ plotting/plot_equatorial_slices.py
@@ -1,15 +1,13 @@
 """
-This script plots snapshots of the evolution of 2D slices from a 2D simulation in polar geometry.
-
-The fields specified in 'fig_type' are plotted (temperature and enstrophy by default).
-To plot a different set of fields, add a new fig type number, and expand the fig_type if-statement.
+This script plots snapshots of the evolution of a 2D slice through the equator of a BallBasis simulation.
 
 Usage:
-    plot_annular_snapshots.py <root_dir> [options]
+    plot_equatorial_slices.py <root_dir> --r_inner=<r> --r_outer=<r> [options]
+    plot_equatorial_slices.py <root_dir> --mesa_file=<f> [options]
 
 Options:
     --data_dir=<dir>                    Name of data handler directory [default: slices]
-    --fig_name=<fig_name>               Name of figure output directory & base name of saved figures [default: snapshots_eq]
+    --fig_name=<fig_name>               Name of figure output directory & base name of saved figures [default: snapshots_equatorial]
     --start_fig=<fig_start_num>         Number of first figure file [default: 1]
     --start_file=<file_start_num>       Number of Dedalus output file to start plotting at [default: 1]
     --n_files=<num_files>               Total number of files to plot
@@ -17,15 +15,6 @@ Options:
 
     --col_inch=<in>                     Number of inches / column [default: 3]
     --row_inch=<in>                     Number of inches / row [default: 3]
-    --static_cbar                       If flagged, don't evolve the colorbar with time
-
-    --radius=<r>                        Max radius of ball
-
-    --fig_type=<fig_type>               Type of figure to plot
-                                            1 - T, u
-                                            2 - u, ω, ωfluc
-                                            3 - partial pressures
-                                        [default: 1]
 """
 from docopt import docopt
 args = docopt(__doc__)
@@ -47,22 +36,26 @@ n_files     = args['--n_files']
 if n_files is not None: 
     n_files = int(n_files)
 
-radius = args['--radius']
-if radius is None:
-    raise ValueError("Must specify outer radius of ball")
-radius = float(radius)
+if args['--r_inner'] is not None and args['--r_outer'] is not None:
+    r_inner = float(args['--r_inner'])
+    r_outer = float(args['--r_outer'])
+else:
+    import h5py
+    with h5py.File(args['--mesa_file'], 'r') as f:
+        r_inner = f['r_inner'][()]
+        r_outer = f['r_outer'][()]
 
 # Create Plotter object, tell it which fields to plot
 plotter = SlicePlotter(root_dir, file_dir=data_dir, fig_name=fig_name, start_file=start_file, n_files=n_files)
-plotter_kwargs = { 'col_in' : int(args['--col_inch']), 'row_in' : int(args['--row_inch']) }
-if int(args['--fig_type']) == 1:
-    plotter.setup_grid(2, 2, polar=True, **plotter_kwargs)
-    fnames = [  (('s1_eq',),         {'polar' : True, 'remove_x_mean' : True, 'divide_x_mean' : True}), 
-                (('u_eq',),         {'polar' : True, 'cmap' : 'PuOr_r', 'vector_ind' : 0}),
-                (('u_eq',),         {'polar' : True, 'cmap' : 'PiYG_r', 'vector_ind' : 1}),
-                (('u_eq',),         {'polar' : True, 'cmap' : 'PuOr_r', 'vector_ind' : 2}),
-             ]
+plotter_kwargs = { 'col_inch' : int(args['--col_inch']), 'row_inch' : int(args['--row_inch']) }
 
-for tup in fnames:
-    plotter.add_colormesh(*tup[0], x_basis='φ', y_basis='r', **tup[1])
-plotter.plot_colormeshes(start_fig=start_fig, dpi=int(args['--dpi']), r_pad=[0, radius])
+# Just plot a single plot (1x1 grid) of the field "T eq"
+# remove_x_mean option removes the (numpy horizontal mean) over phi
+# divide_x_mean divides the radial mean(abs(T eq)) over the phi direction
+plotter.setup_grid(num_rows=2, num_cols=2, polar=True, **plotter_kwargs)
+kwargs = {'azimuth_basis' : 'φ', 'radial_basis' : 'r', 'r_inner' : r_inner, 'r_outer' : r_outer}
+plotter.add_ball_shell_polar_colormesh(ball='s1B_eq', shell='s1S_eq', remove_x_mean=True, divide_x_mean=True, **kwargs)
+plotter.add_ball_shell_polar_colormesh(ball='uB_eq', shell='uS_eq', vector_ind=0, cmap='PuOr_r', **kwargs)
+plotter.add_ball_shell_polar_colormesh(ball='uB_eq', shell='uS_eq', vector_ind=1, cmap='PuOr_r', **kwargs)
+plotter.add_ball_shell_polar_colormesh(ball='uB_eq', shell='uS_eq', vector_ind=2, cmap='PuOr_r', **kwargs)
+plotter.plot_colormeshes(start_fig=start_fig, dpi=int(args['--dpi']))
