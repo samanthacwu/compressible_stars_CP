1d81155435b9ad1a14edb9a33b32b52752821257
Evan H. Anders <evanhanders@gmail.com>
2022-02-07 10:54:21 -0800

    volume visualization modernized and actually looks nice

--- massive_stars/dynamical_simulations/plotting/ballShell_plot_volume_visual.py
+++ massive_stars/dynamical_simulations/plotting/ballShell_plot_volume_visual.py
@@ -16,7 +16,7 @@ import numpy as np
 from docopt import docopt
 args = docopt(__doc__)
 
-from plotpal.file_reader import SingleFiletypePlotter as SFP
+from plotpal.file_reader import SingleTypeReader, match_basis
 
 import plotly
 import plotly.graph_objects as go
@@ -35,7 +35,8 @@ if root_dir is None:
 
 r_outer = float(args['--r_outer'])
 
-plotter = SFP(root_dir, file_dir=data_dir, fig_name='volume_vizualization', start_file=1, n_files=np.inf, distribution='even')
+fig_name='volume_vizualization'
+plotter = SingleTypeReader(root_dir, data_dir, fig_name, start_file=1, n_files=np.inf, distribution='even-write')
 if not plotter.idle:
     phi_keys = []
     theta_keys = []
@@ -48,23 +49,19 @@ if not plotter.idle:
                 theta_keys.append(k)
     r_keys = ['r_0', 'r_1']
 
-    fields = ['s1B(phi=0)', 's1B(phi=pi)', 's1B(r=1)', 's1B_eq', 's1S(phi=0)', 's1S(phi=pi)', 's1S(r=0.95R)', 's1S_eq']
-    bases  = [r_keys[0], r_keys[1], phi_keys[0], theta_keys[0]]
+    fields = ['s1_B(phi=0)', 's1_B(phi=pi)', 's1_B(r=1)', 's1_B_eq', 's1_S(phi=0)', 's1_S(phi=pi)', 's1_S(r=0.95R)', 's1_S_eq']
+    bases  = ['r', 'φ', 'θ']
 
     re = float(root_dir.split('Re')[-1].split('_')[0])
 
-    fig = go.Figure(layout={'width': 2000, 'height': 1000})
+    fig = go.Figure(layout={'width': 1000, 'height': 1000})
     make_subplots(rows=1, cols=1, specs=[[{'is_3d': True}]], subplot_titles=['entropy fluctuations'], figure=fig, horizontal_spacing=0)
     fig.update_annotations(y=0.8, selector={'text':'T'})
     fig.update_layout(scene = {
                         'xaxis': {'showbackground':False, 'tickvals':[], 'title':''}, 
                         'yaxis': {'showbackground':False, 'tickvals':[], 'title':''}, 
                         'zaxis': {'showbackground':False, 'tickvals':[], 'title':''}},
-#                      scene2 = {
-#                        'xaxis': {'showbackground':False, 'tickvals':[], 'title':''}, 
-#                        'yaxis': {'showbackground':False, 'tickvals':[], 'title':''}, 
-#                        'zaxis': {'showbackground':False, 'tickvals':[], 'title':''}},
-                      margin={'l':0, 'r':0, 'b':0, 't':50, 'pad':0}, font={'size' : 18}, annotations={'font' : {'size' : 16}})
+                      margin={'l':0, 'r':0, 'b':0, 't':0, 'pad':0}, font={'size' : 18}, annotations={'font' : {'size' : 16}})
 
     s1_eq_data = OrderedDict()
     s1_mer_data = OrderedDict()
@@ -79,44 +76,51 @@ if not plotter.idle:
     colorbar_dict=dict(lenmode='fraction', len=0.75, thickness=20)
     r_arrays = []
 
-    while plotter.files_remain(bases, fields):
-        bs, tsk, write, time = plotter.read_next_file()
-        plain_theta = bs[theta_keys[0]]
-        plain_phi   = bs[phi_keys[0]]
-        plain_rB     = bs[r_keys[0]]
-        plain_rS     = bs[r_keys[1]]
-        theta = np.pad(plain_theta, ((0,0), (1,1), (0,0)), mode='constant', constant_values=(np.pi, 0))
-        phi   = np.pad(plain_phi, ((0,1), (0,0), (0,0)), mode='constant', constant_values=(2*np.pi))
-        rB     = np.pad(plain_rB, ((0,0), (0,0), (1,0)), mode='constant', constant_values=(0,))
-        rS     = np.pad(plain_rS, ((0,0), (0,0), (0,1)), mode='constant', constant_values=(r_outer,))
-        full_r = np.concatenate((rB, rS), axis=-1)
+    bs = {}
+    while plotter.writes_remain():
+        dsets, ni = plotter.get_dsets(fields)
+        time_data=dsets[fields[0]].dims[0]
+        plain_theta = match_basis(dsets['s1_B(r=1)'], 'θ')[None,:,None]
+        plain_phi   = match_basis(dsets['s1_B(r=1)'], 'φ')[:,None,None]
+        plain_phi_de   = match_basis(dsets['s1_B_eq'], 'φ')[:,None,None]
+        plain_theta_de = match_basis(dsets['s1_B(phi=0)'], 'θ')[None,:,None]
+        plain_rB_de    = match_basis(dsets['s1_B(phi=0)'], 'r')[None,None,:]
+        plain_rS_de    = match_basis(dsets['s1_S(phi=0)'], 'r')[None,None,:]
+        theta  = np.pad(plain_theta, ((0,0), (1,1), (0,0)), mode='constant', constant_values=(np.pi, 0))
+        phi    = np.pad(plain_phi, ((0,1), (0,0), (0,0)), mode='constant', constant_values=(2*np.pi))
+        theta_de  = np.pad(plain_theta_de, ((0,0), (1,1), (0,0)), mode='constant', constant_values=(np.pi, 0))
+        phi_de    = np.pad(plain_phi_de, ((0,1), (0,0), (0,0)), mode='constant', constant_values=(2*np.pi))
+        rB_de     = np.pad(plain_rB_de, ((0,0), (0,0), (1,0)), mode='constant', constant_values=(0,))
+        rS_de     = np.pad(plain_rS_de, ((0,0), (0,0), (0,1)), mode='constant', constant_values=(r_outer,))
+        full_r_de = np.concatenate((rB_de, rS_de), axis=-1)
 
-        mean_s1B = np.expand_dims(np.mean(tsk['s1B_eq'], axis=1), axis=1)
-        mean_s1S = np.expand_dims(np.mean(tsk['s1S_eq'], axis=1), axis=1)
-        tsk['s1B_eq'] -= mean_s1B
-        tsk['s1S_eq'] -= mean_s1S
-        tsk['s1B(r=1)'] -= np.expand_dims(np.mean(np.mean(tsk['s1B(r=1)'], axis=2), axis=1), axis=[1,2])
-        tsk['s1S(r=0.95R)'] -= np.expand_dims(np.mean(np.mean(tsk['s1S(r=0.95R)'], axis=2), axis=1), axis=[1,2])
-        tsk['s1B(phi=0)']  -= mean_s1B 
-        tsk['s1B(phi=pi)'] -= mean_s1B 
-        tsk['s1S(phi=0)']  -= mean_s1S
-        tsk['s1S(phi=pi)'] -= mean_s1S
+        mean_s1_B = np.expand_dims(np.mean(dsets['s1_B_eq'][ni], axis=0), axis=0)
+        mean_s1_S = np.expand_dims(np.mean(dsets['s1_S_eq'][ni], axis=0), axis=0)
+        s1_B_eq = dsets['s1_B_eq'][ni] - mean_s1_B
+        s1_S_eq = dsets['s1_S_eq'][ni] - mean_s1_S
+        s1_B_r1 = dsets['s1_B(r=1)'][ni] - np.expand_dims(np.mean(np.mean(dsets['s1_B(r=1)'][ni], axis=2), axis=1), axis=[1,2])
+        s1_S_r095R = dsets['s1_S(r=0.95R)'][ni] - np.expand_dims(np.mean(np.mean(dsets['s1_S(r=0.95R)'][ni], axis=2), axis=1), axis=[1,2])
+        s1_B_phi0  = dsets['s1_B(phi=0)'][ni] - mean_s1_B 
+        s1_B_phipi = dsets['s1_B(phi=pi)'][ni] - mean_s1_B 
+        s1_S_phi0  = dsets['s1_S(phi=0)'][ni] - mean_s1_S
+        s1_S_phipi = dsets['s1_S(phi=pi)'][ni] - mean_s1_S
         for eq_data in [s1_eq_data]:
-            eq_data['x'] = (full_r*np.cos(phi)).squeeze()
-            eq_data['y'] = (full_r*np.sin(phi)).squeeze()
+            eq_data['x'] = (full_r_de*np.cos(phi_de)).squeeze()
+            eq_data['y'] = (full_r_de*np.sin(phi_de)).squeeze()
             eq_data['z'] = np.zeros_like(eq_data['x'])
 
-        X_mer1 = (full_r*np.cos(0)*np.sin(theta)).squeeze()
-        Y_mer1 = np.zeros_like(X_mer1)
-        Z_mer1 = (full_r*np.cos(theta)).squeeze()
-        X_mer2 = (full_r*np.cos(np.pi)*np.sin(theta)).squeeze()
-        Y_mer2 = np.zeros_like(X_mer2)
-        Z_mer2 = (full_r*np.cos(theta)).squeeze()
+        X_mer1_de = (full_r_de*np.cos(0)*np.sin(theta_de)).squeeze()
+        Y_mer1_de = np.zeros_like(X_mer1_de)
+        Z_mer1_de = (full_r_de*np.cos(theta_de)).squeeze()
+        X_mer2_de = (full_r_de*np.cos(np.pi)*np.sin(theta_de)).squeeze()
+        Y_mer2_de = np.zeros_like(X_mer2_de)
+        Z_mer2_de = (full_r_de*np.cos(theta_de)).squeeze()
+
 
         for mer_data in [s1_mer_data]:
-            mer_data['x'] = np.concatenate((X_mer1, X_mer2))
-            mer_data['y'] = np.concatenate((Y_mer1, Y_mer2))
-            mer_data['z'] = np.concatenate((Z_mer1, Z_mer2))
+            mer_data['x'] = np.concatenate((X_mer1_de, X_mer2_de))
+            mer_data['y'] = np.concatenate((Y_mer1_de, Y_mer2_de))
+            mer_data['z'] = np.concatenate((Z_mer1_de, Z_mer2_de))
 
         for shell_data in [s1_shell_data,]:
             shell_data['x'] = 0.95*r_outer*(np.cos(plain_phi)*np.sin(plain_theta)).squeeze()
@@ -146,39 +150,38 @@ if not plotter.idle:
                 shell_data[k] = np.where(shell_bool, shell_data[k], 0)
 
 
-        for i, n in enumerate(write):
-            print('plotting {}'.format(n))
-            eq_field_s1B = np.pad(tsk['s1B_eq'][i,:].squeeze(), ((0,1), (1,0)), mode='edge')
-            eq_field_s1S = np.pad(tsk['s1S_eq'][i,:].squeeze(), ((0,1), (0,1)), mode='edge')
-            eq_field_s1 = np.concatenate((eq_field_s1B, eq_field_s1S), axis=-1)
-            radial_scaling = np.sqrt(np.expand_dims(np.mean(eq_field_s1**2, axis=0), axis=0))
-            eq_field_s1 /= radial_scaling
-            minmax_s1 = 2*np.std(eq_field_s1)
-            s1_eq_data['surfacecolor'] = eq_field_s1
-            mer_0_s1B = np.pad(tsk['s1B(phi=0)'][i,:].squeeze(), ((1, 1), (1, 0)), mode='edge') 
-            mer_1_s1B = np.pad(tsk['s1B(phi=pi)'][i,:].squeeze(), ((1, 1), (1, 0)), mode='edge') 
-            mer_0_s1S = np.pad(tsk['s1S(phi=0)'][i,:].squeeze(), ((1, 1), (1, 0)), mode='edge') 
-            mer_1_s1S = np.pad(tsk['s1S(phi=pi)'][i,:].squeeze(), ((1, 1), (1, 0)), mode='edge') 
+        eq_field_s1_B = np.pad(s1_B_eq.squeeze(), ((0,1), (1,0)), mode='edge')
+        eq_field_s1_S = np.pad(s1_S_eq.squeeze(), ((0,1), (0,1)), mode='edge')
+        eq_field_s1 = np.concatenate((eq_field_s1_B, eq_field_s1_S), axis=-1)
+        radial_scaling = np.sqrt(np.expand_dims(np.mean(eq_field_s1**2, axis=0), axis=0))
+        eq_field_s1 /= radial_scaling
+        minmax_s1 = 2*np.std(eq_field_s1)
+        s1_eq_data['surfacecolor'] = eq_field_s1
+        mer_0_s1_B = np.pad(s1_B_phi0.squeeze(), ((1, 1), (1, 0)), mode='edge') 
+        mer_1_s1_B = np.pad(s1_B_phipi.squeeze(), ((1, 1), (1, 0)), mode='edge') 
+        mer_0_s1_S = np.pad(s1_S_phi0.squeeze(), ((1, 1), (1, 0)), mode='edge') 
+        mer_1_s1_S = np.pad(s1_S_phipi.squeeze(), ((1, 1), (1, 0)), mode='edge') 
 
-            mer_0_s1 = np.concatenate((mer_0_s1B, mer_0_s1S), axis=-1)/radial_scaling
-            mer_1_s1 = np.concatenate((mer_1_s1B, mer_1_s1S), axis=-1)/radial_scaling
-            mer_s1   = np.concatenate((mer_0_s1, mer_1_s1))
+        mer_0_s1 = np.concatenate((mer_0_s1_B, mer_0_s1_S), axis=-1)/radial_scaling
+        mer_1_s1 = np.concatenate((mer_1_s1_B, mer_1_s1_S), axis=-1)/radial_scaling
+        mer_s1   = np.concatenate((mer_0_s1, mer_1_s1))
 
-            s1_mer_data['surfacecolor'] = mer_s1
-            shell_s1 = tsk['s1S(r=0.95R)'][i,:].squeeze()
-            shell_s1 /= np.sqrt(np.mean(shell_s1**2))
-            s1_shell_data['surfacecolor'] = np.where(shell_bool, shell_s1, 0)
+        s1_mer_data['surfacecolor'] = mer_s1
+        shell_s1 = s1_S_r095R.squeeze()
+        shell_s1 /= np.sqrt(np.mean(shell_s1**2))
+        s1_shell_data['surfacecolor'] = np.where(shell_bool, shell_s1, 0)
 
-            fig.add_trace(go.Surface(**s1_shell_data, colorbar_x=-0.07, cmin=-minmax_s1, cmax=minmax_s1, colorscale='RdBu_r', colorbar=colorbar_dict), 1, 1)
-            fig.add_trace(go.Surface(**s1_mer_data, colorbar_x=-0.07,   cmin=-minmax_s1, cmax=minmax_s1,   colorscale='RdBu_r', colorbar=colorbar_dict), 1, 1)
-            fig.add_trace(go.Surface(**s1_eq_data, colorbar_x=-0.07,    cmin=-minmax_s1, cmax=minmax_s1,    colorscale='RdBu_r', colorbar=colorbar_dict), 1, 1)
+        fig.add_trace(go.Surface(**s1_shell_data, colorbar_x=-0.07, cmin=-minmax_s1, cmax=minmax_s1, colorscale='RdBu_r', colorbar=colorbar_dict), 1, 1)
+        fig.add_trace(go.Surface(**s1_mer_data, colorbar_x=-0.07,   cmin=-minmax_s1, cmax=minmax_s1,   colorscale='RdBu_r', colorbar=colorbar_dict), 1, 1)
+        fig.add_trace(go.Surface(**s1_eq_data, colorbar_x=-0.07,    cmin=-minmax_s1, cmax=minmax_s1,    colorscale='RdBu_r', colorbar=colorbar_dict), 1, 1)
 
-            for l in lines:
-                fig.add_trace(go.Scatter3d(**l, mode='lines', line={'color':'black'}, showlegend=False), 1, 1)
+        for l in lines:
+            fig.add_trace(go.Scatter3d(**l, mode='lines', line={'color':'black'}, showlegend=False), 1, 1)
 
-            title_text = 't = {:.3e}'.format(time[i])
-            fig.update_layout(title=title_text)
-            fig.update_annotations(y=0.85, selector={'text':title_text})
-            figname = '{:s}/plotly3Dplot/plotly3Dplot_{:06d}.png'.format(root_dir, n)
-            pio.write_image(fig, figname, format='png', engine='kaleido', scale=float(args['--scale']))
-            fig.data = []
+        title_text = 't = {:.3e}'.format(time_data['sim_time'][ni])
+        viewpoint = {'camera_eye' : {'x' : 0.7, 'y': 1.1, 'z' : 1}}
+        fig.update_layout(title=title_text, scene=viewpoint)
+        fig.update_annotations(y=0.85, selector={'text':title_text})
+        this_fig_name = '{:s}/{:s}_{:06d}.png'.format(plotter.out_dir, fig_name, time_data['write_number'][ni])
+        pio.write_image(fig, this_fig_name, format='png', engine='kaleido', scale=float(args['--scale']))
+        fig.data = []
