b6e5f03cbecb02ad380fb013b7256c71fadcd4d2
Evan H. Anders <evanhanders@gmail.com>
2022-03-01 15:09:21 -0800

    shell problem properly has tau_p failure form u dot grad s0 term; why??

--- massive_stars/dynamical_simulations/minimal_shell_AN.py
+++ massive_stars/dynamical_simulations/minimal_shell_AN.py
@@ -12,7 +12,7 @@ Options:
     --Re=<Re>            The Reynolds number of the numerical diffusivities [default: 2e2]
     --Pr=<Prandtl>       The Prandtl number  of the numerical diffusivities [default: 1]
     --ntheta=<res>       Number of theta grid points (Lmax+1)   [default: 4]
-    --nr=<res>          Number of radial grid points in shell (Nmax+1)   [default: 8]
+    --nr=<res>          Number of radial grid points in ball (Nmax+1)   [default: 24]
 
     --wall_hours=<t>     Max number of wall hours to run simulation for [default: 24]
     --buoy_end_time=<t>  Max number of buoyancy time units to simulate [default: 1e5]
@@ -63,8 +63,8 @@ if args['<config>'] is not None:
 # Parameters
 nθ  = int(args['--ntheta'])
 nφ  = int(2*nθ)
-nrS = int(args['--nr'])
-resolutionS = (nφ, nθ, nrS)
+nr = int(args['--nr'])
+resolutionS = (nφ, nθ, nr)
 L_dealias = N_dealias = dealias = 1.5
 dtype = np.float64
 Re  = float(args['--Re'])
@@ -99,8 +99,6 @@ if ncc_file is not None:
 else:
     r_inner = 1.1
     r_outer = 1.5
-r_inner = 1.0
-r_outer = 3.0
 logger.info('r_inner: {:.2f} / r_outer: {:.2f}'.format(r_inner, r_outer))
 
 # Bases
@@ -156,7 +154,7 @@ tensor_nccs = ['I_matrix']
 vec_nccs = ['grad_ln_ρ', 'grad_ln_T', 'grad_s0', 'grad_T', 'grad_inv_Pe_rad']
 scalar_nccs = ['ln_ρ', 'ln_T', 'inv_Pe_rad']
 
-for basis, name in zip((shell_basis,), ('S')):
+for basis, name in zip((shell_basis,), ('S',)):
     for fn in tensor_nccs:
         key = '{}_{}'.format(fn, name)
         ncc_dict[key] = dist.TensorField(coords, name=key, bases=basis.radial_basis)
@@ -166,15 +164,11 @@ for basis, name in zip((shell_basis,), ('S')):
     for fn in scalar_nccs:
         key = '{}_{}'.format(fn, name)
         ncc_dict[key] = dist.Field(name=key, bases=basis.radial_basis)
-ncc_dict['T_NCC'] = dist.Field(name='T_NCC', bases=shell_basis.radial_basis)
 
 for k in ncc_dict.keys():
     ncc_dict[k]['g'][:] = 0
 
-ncc_dict['rvec'] = dist.VectorField(coords, name='T_NCC', bases=shell_basis.radial_basis)
-ncc_dict['rvec']['g'][2] = r1S
-
-for basis_name in ['S']:
+for basis_name in ['S',]:
     for i in range(3):
         ncc_dict['I_matrix_{}'.format(basis_name)]['g'][i,i,:] = 1
 
@@ -203,9 +197,6 @@ if ncc_file is not None:
         field_dict['ρ_S']['g']         = np.exp(f['ln_ρS'][:,:,grid_slices_S[-1]])[None,None,:]
         field_dict['T_S']['g']         = f['TS'][:,:,grid_slices_S[-1]][None,None,:]
         field_dict['inv_T_S']['g']     = 1/field_dict['T_S']['g']
-
-        ncc_dict['T_NCC'].change_scales(basis.dealias)
-        ncc_dict['T_NCC']['g'] = f['TS'][:,:,grid_slices_S[-1]]
 
         max_dt = f['max_dt'][()]
         t_buoy = 1 #assume nondimensionalization on heating ~ buoyancy time
@@ -245,28 +236,17 @@ else:
 # Put nccs and fields into locals()
 locals().update(ncc_dict)
 locals().update(field_dict)
-max_dt /= 1
-
-ncc_mag = 1e4
-ncc_pow = 10
-#grad_s0_S['g'] = ncc_mag*(((rS - r_inner)/(r_outer - r_inner))**ncc_pow + 1)
-logger.info("NCC expansions:")
-ncc_cutoff=1e-6
-for ncc in [rvec, grad_s0_S]:
-        logger.info("{}: {}".format(ncc, np.where(np.abs(ncc['c']) >= ncc_cutoff)[0].shape))
 
 # Lift operators for boundary conditions
-lift_k2_basis = shell_basis.clone_with(k=2)
-lift_k2   = lambda A, n: d3.Lift(A, lift_k2_basis, n)
-lift_shell_basis = shell_basis.clone_with(k=1)
+lift_shell_basis = shell_basis.clone_with(k=2)
 liftS   = lambda A, n: d3.Lift(A, lift_shell_basis, n)
 integ     = lambda A: d3.Integrate(A, coords)
-BC_u_S = lift_k2(tau_u_Stop, -1) + lift_k2(tau_u_Sbot, -2)
-BC_s1_S = lift_k2(tau_s_Stop, -1) + lift_k2(tau_s_Sbot, -2)
+BC_u_S = liftS(tau_u_Stop, -1) + liftS(tau_u_Sbot, -2)
+BC_s1_S = liftS(tau_s_Stop, -1) + liftS(tau_s_Sbot, -2)
 
 #Stress matrices & viscous terms (assumes uniform kinematic viscosity; so dynamic viscosity mu = const * rho)
 divU_S = d3.div(u_S)
-grad_u_S = d3.grad(u_S) + rvec*liftS(tau_u_Sbot, -1)
+grad_u_S = d3.grad(u_S)
 E_S = 0.5*(d3.grad(u_S) + d3.transpose(d3.grad(u_S)))
 σ_S = 2*(E_S - (1/3)*divU_S*I_matrix_S)
 visc_div_stress_S = d3.div(σ_S) + d3.dot(σ_S, grad_ln_ρ_S)
@@ -275,49 +255,56 @@ VH_S  = 2*(d3.trace(d3.dot(E_S, E_S)) - (1/3)*divU_S*divU_S)
 # Grid-lock some operators / define grad's
 H_S = d3.Grid(H_S).evaluate()
 inv_T_S = d3.Grid(inv_T_S).evaluate()
-grad_s1_S = d3.grad(s1_S) + rvec*liftS(tau_s_Sbot, -1)
+grad_s1_S = d3.grad(s1_S)
 
+#inv_Pe_rad_S['g'] = 1/Re
 #div_rad_flux_S = (inv_Pe_rad_S)*(d3.div(grad_s1_S) + d3.dot(grad_s1_S, (grad_ln_ρ_S + grad_ln_T_S))) + d3.dot(grad_s1_S, d3.grad(inv_Pe_rad_S))
-#div_rad_flux_S = (inv_Pe_rad_S)*(d3.div(grad_s1_S + rvec*liftS(tau_s_Sbot, -1)))
-div_rad_flux_S = (inv_Pe_rad_S)*(d3.div(grad_s1_S))
-div_rad_flux_S += d3.dot(grad_s1_S, d3.grad(inv_Pe_rad_S))
-div_rad_flux_S += inv_Pe_rad_S*d3.dot(grad_s1_S, (grad_ln_ρ_S+ grad_ln_T_S)) 
+div_rad_flux_S = (1/Re)*d3.lap(s1_S)
+
+grad_ln_ρ_S['g'] = 0
+
+grad_ln_T_S['g'] = 0
+
+visc_div_stress_S = d3.lap(u_S)
+
+grad_T_S['g'] = 0
+
+grad_s0_S['g'] = 0
+for ncc_mag, ncc_order in zip((1e5, 1e5), (3, 31)):
+    grad_s0_S['g'][2] += ncc_mag* ( 1 + ((rS-r_inner)/(r_outer-r_inner))**ncc_order)
+ncc_cutoff=1e-10
+logger.info("{} expansion: {} / goal: {}".format(grad_s0_S, np.where(np.abs(grad_s0_S['c']) >= ncc_cutoff)[0].shape, ncc_order + 1)) 
 
 # Problem
-problem = d3.IVP([p_S, s1_S, u_S, tau_p_S, tau_s_Sbot, tau_s_Stop, tau_u_Sbot, tau_u_Stop, ], namespace=locals())
+problem = d3.IVP([p_S, u_S, s1_S, tau_p_S, tau_u_Sbot, tau_u_Stop, tau_s_Sbot, tau_s_Stop], namespace=locals())
 
-#FOF
-problem.add_equation("trace(grad_u_S) + dot(u_S, grad_ln_ρ_S) + tau_p_S = 0")
-problem.add_equation("dt(s1_S) + dot(u_S, grad_s0_S) - (1/Re)*div(grad_s1_S) + liftS(tau_s_Stop, -1) = 0 ")
-problem.add_equation("dt(u_S) + grad(p_S) + grad_T_S*s1_S - (1/Re)*div(grad_u_S) + liftS(tau_u_Stop, -1) = 0")
+problem.add_equation("div(u_S) + tau_p_S = 0")
+problem.add_equation("dt(u_S) + grad(p_S) + grad_T_S*s1_S - (1/Re)*visc_div_stress_S + BC_u_S = 0")
+problem.add_equation("dt(s1_S) + dot(u_S, grad_s0_S) - div_rad_flux_S + BC_s1_S = 0 ")
 
-##k2 formalism
-#problem.add_equation("div(u_S) + dot(u_S, grad_ln_ρ_S) + tau_p_S = 0")
-#problem.add_equation("dt(s1_S) + dot(u_S, grad_s0_S) - (1/Re)*lap(s1_S) + BC_s1_S = 0 ")
-#problem.add_equation("dt(u_S) + grad(p_S) + grad_T_S*s1_S - (1/Re)*lap(u_S) + BC_u_S = 0")
-
-
-problem.add_equation("s1_S(r=r_inner) = 0")
+problem.add_equation("u_S(r=r_outer) = 0")
 problem.add_equation("u_S(r=r_inner) = 0")
 problem.add_equation("s1_S(r=r_outer) = 0")
-problem.add_equation("u_S(r=r_outer) = 0")
+problem.add_equation("s1_S(r=r_inner) = 0")
+problem.add_equation("integ(p_S) = 0")
 
-#problem.add_equation("radial(u_S(r=r_inner)) = 0")
-#problem.add_equation("angular(radial(E_S(r=r_inner))) = 0")
 #problem.add_equation("radial(u_S(r=r_outer)) = 0")
 #problem.add_equation("angular(radial(E_S(r=r_outer))) = 0")
-
-# Entropy BCs
-#problem.add_equation("radial(grad_s1_S(r=r_inner)) = 0")
+#problem.add_equation("radial(u_S(r=r_inner)) = 0")
+#problem.add_equation("angular(radial(E_S(r=r_inner))) = 0")
+#
+## Entropy BCs
 #problem.add_equation("radial(grad_s1_S(r=r_outer)) = 0")
-
-problem.add_equation("integ(p_S) = 0")
+#problem.add_equation("radial(grad_s1_S(r=r_inner)) = 0")
+#
+#problem.add_equation("integ(p_S) = 0")
 
 logger.info("Problem built")
 # Solver
 solver = problem.build_solver(ts, ncc_cutoff=ncc_cutoff)
 solver.stop_sim_time = buoy_end_time*t_buoy
 solver.stop_wall_time = wall_hours * 60 * 60
+solver.stop_iteration = 100
 logger.info("solver built")
 
 # Initial conditions / Checkpoint
@@ -328,22 +315,22 @@ if restart is not None:
     write_mode = 'append'
 else:
     # Initial conditions
-    s1_S.fill_random(layout='g', seed=42, distribution='normal', scale=1)
+    s1_S.fill_random(layout='g', seed=42, distribution='normal', scale=A0)
     s1_S.low_pass_filter(scales=0.25)
-    s1_S['g'] *= (r1S - r_inner) * (r_outer - r1S) # Damp noise at walls
-#    s1_S['g'] *= zero_to_one(r1S, r_inner+0.3, width=0.1)*one_to_zero(r1S, r_outer-0.3, width=0.1)
-u_S['g'] = 0
+    s1_S['g'] *= (r1S - r_inner)*(r_outer - r1S)*np.sin(θ1S)
 
 # Averaging Operations
 volume_S = (4/3)*np.pi*(r_outer**3 - r_inner**3)
 vol_avg_S = lambda A: d3.Integrate(A/volume_S, coords)
 u_S_squared = d3.dot(u_S, u_S)
 
-re_shell = solver.evaluator.add_dictionary_handler(iter=1)
-re_shell.add_task(vol_avg_S(Re*(u_S_squared)**(1/2)), name='Re_avg_shell', layout='g')
+re_eval = solver.evaluator.add_dictionary_handler(iter=1)
+re_eval.add_task(vol_avg_S(Re*(u_S_squared)**(1/2)), name='Re_avg_shell', layout='g')
 
 #CFL setup
 initial_max_dt = max_dt
+while initial_max_dt < max_dt:
+    max_dt /= 2
 if timestep is None:
     timestep = initial_max_dt
 my_cfl = d3.CFL(solver, timestep, safety=safety, cadence=1, max_dt=initial_max_dt, min_change=0.1, max_change=1.5, threshold=0.1)
@@ -364,16 +351,16 @@ try:
         solver.step(timestep)
 
         if solver.iteration % 1 == 0:
-            Re_avg = re_shell.fields['Re_avg_shell']
+            Re_avg = re_eval.fields['Re_avg_shell']
             if dist.comm_cart.rank == 0:
                 Re0 = Re_avg['g'].min()
-                tau_p = tau_p_S['g'].squeeze()
+                taus = tau_p_S['g'].squeeze()
             else:
                 Re0 = None
-                tau_p = -1
+                taus = -1
             Re0 = dist.comm_cart.bcast(Re0, root=0)
             this_str = "t = {:f}, timestep = {:f}, Re = {:.4e}".format(solver.sim_time, timestep, Re0)
-            this_str += ", tau_ps = {:.4e}".format(tau_p)
+            this_str += ", tau_ps = ({:.4e})".format(taus)
             logger.info(this_str)
 
         if np.isnan(Re0):
@@ -384,17 +371,4 @@ except:
     logger.info('something went wrong in main loop, making final checkpoint')
     raise
 finally:
-    end_time = time.time()
-    end_iter = solver.iteration
-    cpu_sec  = end_time - start_time
-    n_iter   = end_iter - start_iter
-
-
-    #TODO: Make the end-of-sim report better
-    n_coeffs = np.prod(resolutionS)
-    n_cpu    = dist.comm_cart.size
-    dof_cycles_per_cpusec = n_coeffs*n_iter/(cpu_sec*n_cpu)
-    logger.info('DOF-cycles/cpu-sec : {:e}'.format(dof_cycles_per_cpusec))
-    logger.info('Run iterations: {:e}'.format(n_iter))
-    logger.info('Sim end time: {:e}'.format(solver.sim_time))
-    logger.info('Run time: {}'.format(cpu_sec))
+    solver.log_stats()
