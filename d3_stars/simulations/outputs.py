09f31189a1ba5d0d88ba87a097f2ecebaee0a76a
Evan Anders <evanhanders@gmail.com>
2022-06-07 16:45:35 -0500

    grad_ln_rho now consistently conserves energy; need to build all nccs consistently in star_builder.

--- d3_stars/simulations/outputs.py
+++ d3_stars/simulations/outputs.py
@@ -33,7 +33,7 @@ output_tasks['KE_lum']   = '(4*np.pi*r_vals_{0}**2)*(u_{0}*(' + output_tasks['KE
 output_tasks['TE_lum']   = '(4*np.pi*r_vals_{0}**2)*(u_{0}*(' + output_tasks['TE'] + '))'
 output_tasks['wave_lum'] = '(4*np.pi*r_vals_{0}**2)*(u_{0}*rho_{0}*(' + output_tasks['pomega_hat'] + '))'
 output_tasks['visc_lum'] = '(4*np.pi*r_vals_{0}**2)*(-rho_{0}*nu_diff_{0}*(dot(u_{0}, sigma_RHS_{0})))'
-output_tasks['cond_lum'] = '(4*np.pi*r_vals_{0}**2)*(-rho_{0}*g_phi_{0}*chi_rad_{0}*grad(s1_{0}))'
+output_tasks['cond_lum'] = '(4*np.pi*r_vals_{0}**2)*(-rho_{0}*g_phi_{0}*chi_rad_{0}*grad_s1_{0})'
 
 for lum in ['KE_lum', 'TE_lum', 'wave_lum', 'visc_lum', 'cond_lum']:
     output_tasks['{}_r'.format(lum)] = 'dot(er_{0}, ' + output_tasks[lum] + ')'
