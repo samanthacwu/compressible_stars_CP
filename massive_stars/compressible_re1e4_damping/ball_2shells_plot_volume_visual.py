5ad562f0c8b5d296569a5b1d359c7213481e3c6c
Evan Anders <evanhanders@gmail.com>
2022-09-13 15:03:57 -0500

    fixes pole artifact and memory leak in volume viz

--- massive_stars/compressible_re1e4_damping/ball_2shells_plot_volume_visual.py
+++ massive_stars/compressible_re1e4_damping/ball_2shells_plot_volume_visual.py
@@ -9,8 +9,10 @@ Options:
     --scale=<s>          resolution scale factor [default: 1]
     --start_file=<n>     start file number [default: 1]
 """
+import gc
 from collections import OrderedDict
 
+from mpi4py import MPI
 import h5py
 import numpy as np
 from docopt import docopt
@@ -90,6 +92,10 @@ if not plotter.idle:
     ax = fig.add_axes([0, 0, 1, 0.9],projection='3d')
     cax = fig.add_axes([0.05, 0.95, 0.9, 0.04])
 
+    dummy_fig = plt.figure(figsize=(4,4))
+    dummy_ax  = dummy_fig.add_axes([0, 0, 1, 0.9],projection='3d')
+    dummy_cax = dummy_fig.add_axes([0.05, 0.95, 0.9, 0.04])
+
 #    line_ax1 = fig.add_axes([0.18,0.04,0.3, 0.08])
 #    line_ax2 = fig.add_axes([0.68,0.04,0.3, 0.08])
 
@@ -116,70 +122,82 @@ if not plotter.idle:
     while plotter.writes_remain():
         dsets, ni = plotter.get_dsets(fields)
         time_data = plotter.current_file_handle['scales']
-        theta = match_basis(dsets[shell_field], 'theta')
-        phi   = match_basis(dsets[shell_field], 'phi')
-        phi_de   = match_basis(dsets['equator(s1_B)'], 'phi')
-        theta_de = match_basis(dsets['meridian(s1_B,phi=0)'], 'theta')
-        rB_de    = match_basis(dsets['meridian(s1_B,phi=0)'], 'r')
-        rS1_de   = match_basis(dsets['meridian(s1_S1,phi=0)'], 'r')
-        rS2_de   = match_basis(dsets['meridian(s1_S2,phi=0)'], 'r')
-        r_de = r_de_orig = np.concatenate((rB_de, rS1_de, rS2_de), axis=-1)
-        dphi = phi[1] - phi[0]
-        dphi_de = phi_de[1] - phi_de[0]
-
-        if r_max is None:
-            r_outer = r_de.max()
-        else:
-            r_outer = r_max
-            r_de = r_de[r_de <= r_max]
-
-        phi_vert, theta_vert, r_vert = build_spherical_vertices(phi, theta, r_de, 0, r_outer)
-        phi_vert_de, theta_vert_de, r_vert_de = build_spherical_vertices(phi_de, theta_de, r_de, 0, r_outer)
-
-        shell_frac = 0.999
-        shell1_theta_pick = theta_vert >= np.pi/2
-        shell2_theta_pick = np.isfinite(theta_vert)
-        shell1_phi_pick = phi_vert <= np.pi*1.05
-        shell2_phi_pick = phi_vert > np.pi 
-        eq_phi_pick = phi_vert_de <= np.pi
-        xo, yo, zo = spherical_to_cartesian(phi_vert[shell1_phi_pick], theta_vert[shell1_theta_pick], [shell_frac*r_outer])[:,:,:,0]
-        xo2, yo2, zo2 = spherical_to_cartesian(phi_vert[shell2_phi_pick], theta_vert[shell2_theta_pick], [shell_frac*r_outer])[:,:,:,0]
-        xeq, yeq, zeq = spherical_to_cartesian(phi_vert_de[eq_phi_pick], [theta_eq], r_vert_de)[:,:,0,:]
+        print('got time data')
 
 
-        theta_mer = np.concatenate([-theta_de, theta_de[::-1]])
-        mer_theta_pick = np.abs(theta_mer) <= np.pi
-        x_mer, y_mer, z_mer = spherical_to_cartesian([phi_mer1,], theta_mer[mer_theta_pick], r_vert_de)[:,0,:,:]
+        #Only get grid info on first pass
+        if first:
+            theta = match_basis(dsets[shell_field], 'theta')
+            phi   = match_basis(dsets[shell_field], 'phi')
+            phi_de   = match_basis(dsets['equator(s1_B)'], 'phi')
+            theta_de = match_basis(dsets['meridian(s1_B,phi=0)'], 'theta')
+            rB_de    = match_basis(dsets['meridian(s1_B,phi=0)'], 'r')
+            rS1_de   = match_basis(dsets['meridian(s1_S1,phi=0)'], 'r')
+            rS2_de   = match_basis(dsets['meridian(s1_S2,phi=0)'], 'r')
+            r_de = r_de_orig = np.concatenate((rB_de, rS1_de, rS2_de), axis=-1)
+            dphi = phi[1] - phi[0]
+            dphi_de = phi_de[1] - phi_de[0]
 
-        s1_mer_data['x'] = x_mer
-        s1_mer_data['y'] = y_mer
-        s1_mer_data['z'] = z_mer
+            if r_max is None:
+                r_outer = r_de.max()
+            else:
+                r_outer = r_max
+                r_de = r_de[r_de <= r_max]
 
-        s1_shell1_data['x'] = xo
-        s1_shell1_data['y'] = yo
-        s1_shell1_data['z'] = zo
-        s1_shell2_data['x'] = xo2
-        s1_shell2_data['y'] = yo2
-        s1_shell2_data['z'] = zo2
+            phi_vert, theta_vert, r_vert = build_spherical_vertices(phi, theta, r_de, 0, r_outer)
+            phi_vert_de, theta_vert_de, r_vert_de = build_spherical_vertices(phi_de, theta_de, r_de, 0, r_outer)
 
-        #aim for a cutout where x > 0.
-        s1_eq_data['x'] = xeq
-        s1_eq_data['y'] = yeq
-        s1_eq_data['z'] = zeq
- 
-        #Get mean properties as f(radius) // Equatorial data
-        mean_s1_B  = np.expand_dims(np.mean(dsets['equator(s1_B)'][ni], axis=0), axis=0)
-        mean_s1_S1 = np.expand_dims(np.mean(dsets['equator(s1_S1)'][ni], axis=0), axis=0)
-        mean_s1_S2 = np.expand_dims(np.mean(dsets['equator(s1_S2)'][ni], axis=0), axis=0)
-        s1_eq_B  = dsets['equator(s1_B)'][ni] - mean_s1_B
-        s1_eq_S1 = dsets['equator(s1_S1)'][ni] - mean_s1_S1
-        s1_eq_S2 = dsets['equator(s1_S2)'][ni] - mean_s1_S2
-        radial_s1_mean = np.concatenate((mean_s1_B, mean_s1_S1, mean_s1_S2), axis=-1)
-        eq_field_s1 = np.concatenate((s1_eq_B, s1_eq_S1, s1_eq_S2), axis=-1)
-        radial_scaling = np.sqrt(np.mean(eq_field_s1**2, axis=0))
-        eq_field_s1 /= radial_scaling
-        minmax_s1 = 2*np.std(eq_field_s1)
-        s1_eq_data['surfacecolor'] = np.pad(eq_field_s1.squeeze()[:, r_de_orig <= r_outer], ( (1, 0), (1, 0) ), mode='edge')[eq_phi_pick,:]
+            shell_frac = 1 
+            shell1_theta_pick = theta_vert >= np.pi/2
+            shell2_theta_pick = np.isfinite(theta_vert)
+            shell1_theta_pick_field = theta >= np.pi/2
+            shell2_theta_pick_field = np.isfinite(theta)
+            shell1_phi_pick = phi_vert <= np.pi*1.05
+            shell2_phi_pick = phi_vert > np.pi 
+            shell1_phi_pick_field = phi <= np.pi*1.05
+            shell2_phi_pick_field = phi > np.pi 
+            eq_phi_pick = phi_vert_de <= np.pi
+            xo, yo, zo = spherical_to_cartesian(phi_vert[shell1_phi_pick], theta_vert[shell1_theta_pick], [shell_frac*r_outer])[:,:,:,0]
+            xo2, yo2, zo2 = spherical_to_cartesian(phi_vert[shell2_phi_pick], theta_vert[shell2_theta_pick], [shell_frac*r_outer])[:,:,:,0]
+            xeq, yeq, zeq = spherical_to_cartesian(phi_vert_de[eq_phi_pick], [theta_eq], r_vert_de)[:,:,0,:]
+
+
+            theta_mer = np.concatenate([-theta_de, theta_de[::-1]])
+            mer_theta_pick = np.abs(theta_mer) <= np.pi
+            mer_theta_pick = np.abs(theta_mer) <= np.pi
+            x_mer, y_mer, z_mer = spherical_to_cartesian([phi_mer1,], theta_mer[mer_theta_pick], r_vert_de)[:,0,:,:]
+
+            s1_mer_data['x'] = x_mer
+            s1_mer_data['y'] = y_mer
+            s1_mer_data['z'] = z_mer
+
+            s1_shell1_data['x'] = xo
+            s1_shell1_data['y'] = yo
+            s1_shell1_data['z'] = zo
+            s1_shell2_data['x'] = xo2
+            s1_shell2_data['y'] = yo2
+            s1_shell2_data['z'] = zo2
+
+            #aim for a cutout where x > 0.
+            s1_eq_data['x'] = xeq
+            s1_eq_data['y'] = yeq
+            s1_eq_data['z'] = zeq
+        print('past if statement')
+
+        if first:
+            #Get mean properties as f(radius) // Equatorial data
+            mean_s1_B  = np.expand_dims(np.mean(dsets['equator(s1_B)'][ni], axis=0), axis=0)
+            mean_s1_S1 = np.expand_dims(np.mean(dsets['equator(s1_S1)'][ni], axis=0), axis=0)
+            mean_s1_S2 = np.expand_dims(np.mean(dsets['equator(s1_S2)'][ni], axis=0), axis=0)
+            s1_eq_B  = dsets['equator(s1_B)'][ni] - mean_s1_B
+            s1_eq_S1 = dsets['equator(s1_S1)'][ni] - mean_s1_S1
+            s1_eq_S2 = dsets['equator(s1_S2)'][ni] - mean_s1_S2
+            radial_s1_mean = np.concatenate((mean_s1_B, mean_s1_S1, mean_s1_S2), axis=-1)
+            eq_field_s1 = np.concatenate((s1_eq_B, s1_eq_S1, s1_eq_S2), axis=-1)
+            radial_scaling = np.sqrt(np.mean(eq_field_s1**2, axis=0))
+            eq_field_s1 /= radial_scaling
+            s1_eq_data['surfacecolor'] = np.pad(eq_field_s1.squeeze()[:, r_de_orig <= r_outer], ( (1, 0), (1, 0) ), mode='edge')[eq_phi_pick,:]
+            print('past equator')
 
 
         #Get meridional slice data
@@ -201,51 +219,46 @@ if not plotter.idle:
         mer_s1 = np.concatenate( (mer_0_s1, mer_1_s1[::-1,:]), axis=0)
         mer_s1 = np.pad(mer_s1, ((0, 0), (1, 1)), mode='edge')
         s1_mer_data['surfacecolor'] = mer_s1[mer_theta_pick,:]
+        print('past meridian')
 
         #Get shell slice data
-        s1_S_r095R = dsets[shell_field][ni] - np.expand_dims(np.mean(np.mean(dsets[shell_field][ni], axis=2), axis=1), axis=[1,2])
+        s1_S_r095R = dsets[shell_field][ni]
         shell_s1 = s1_S_r095R.squeeze()
-        shell_s1 /= np.sqrt(np.mean(shell_s1**2))
-        shell_s1 = np.pad(shell_s1, ((0, 1), (1, 0)), mode='edge')
-        s1_shell1_data['surfacecolor'] = shell_s1[shell1_phi_pick,:][:,shell1_theta_pick]
-        s1_shell2_data['surfacecolor'] = shell_s1[shell2_phi_pick,:][:,shell2_theta_pick]
+        shell_s1 -= np.mean(shell_s1)
+        shell_s1 /= np.std(shell_s1)
+        s1_shell1_data['surfacecolor'] = shell_s1[shell1_phi_pick_field,:][:,shell1_theta_pick_field]
+        s1_shell2_data['surfacecolor'] = shell_s1[shell2_phi_pick_field,:][:,shell2_theta_pick_field]
+        print('past shell')
 
-        cmap = matplotlib.cm.get_cmap('RdBu_r')
-        norm = matplotlib.colors.Normalize(vmin=-minmax_s1, vmax=minmax_s1)
+        if first: #static colorbar
+            minmax_s1 = np.array((2*np.std(eq_field_s1),))
+            plotter.comm.Allreduce(MPI.IN_PLACE, minmax_s1, op=MPI.MAX)
+            cmap = matplotlib.cm.get_cmap('RdBu_r')
+            norm = matplotlib.colors.Normalize(vmin=-minmax_s1[0], vmax=minmax_s1[0])
 
-#        data_dicts = [s1_shell1_data, s1_shell2_data, s1_mer_data, s1_eq_data]
-        data_dicts = [s1_mer_data, s1_eq_data, s1_shell2_data, s1_shell1_data]
+        data_dicts = [s1_shell1_data, s1_shell2_data, s1_mer_data, s1_eq_data]
+#        data_dicts = [s1_mer_data, s1_eq_data, s1_shell2_data, s1_shell1_data]
+#        data_dicts = [s1_eq_data]
         
-        if first:
-            for i, d in enumerate(data_dicts):
-                print('plotting data {}'.format(i))
-                x = d['x']
-                y = d['y']
-                z = d['z']
-                sfc = cmap(norm(d['surfacecolor']))
-                surf = ax.plot_surface(x, y, z, facecolors=sfc, cstride=1, rstride=1, linewidth=0, antialiased=False, shade=False)
-                d['surf'] = surf
-                #wireframe doesn't seem to be working
-#                ax.plot_wireframe(x, y, z, ccount=1, rcount=1, linewidth=1, color='black')
+        for i, d in enumerate(data_dicts):
+            print('plotting data {}'.format(i))
+            x = d['x']
+            y = d['y']
+            z = d['z']
+            sfc = cmap(norm(d['surfacecolor']))
+            surf = ax.plot_surface(x, y, z, facecolors=sfc, cstride=1, rstride=1, linewidth=0, antialiased=False, shade=False)
+            d['surf'] = surf
+
+            if first:
+                cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap, norm=norm, orientation='horizontal', format=FormatStrFormatter('%.1f'))
+                cbar.set_label('normalized s1' + ', t = {:.3e}'.format(time_data['sim_time'][ni]))
+            ax.view_init(azim=125, elev=25)
             ax.set_xlim(-0.7*r_outer, 0.7*r_outer)
             ax.set_ylim(-0.7*r_outer, 0.7*r_outer)
             ax.set_zlim(-0.7*r_outer, 0.7*r_outer)
-#            ax.set_xlabel('x')
-#            ax.set_ylabel('y')
-#            ax.set_zlabel('z')
             ax.axis('off')
-            ax.view_init(azim=125, elev=35)
-            first = False
-        else:
-            for i, d in enumerate(data_dicts):
-                sfc = cmap(norm(d['surfacecolor']))
-                d['surf'].set_facecolors(sfc.reshape(sfc.size//4,4))
-        cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap, norm=norm, orientation='horizontal', format=FormatStrFormatter('%.1f'))
-        cbar.set_label('normalized s1' + ', t = {:.3e}'.format(time_data['sim_time'][ni]))
-#        cbar.set_ticklabels(["{:.1f}".format(i) for i in cbar.get_ticks()])
-        #TODO: fix colorbar tick flickering when plotting in parallel
-
         fig.savefig('{:s}/{:s}_{:06d}.png'.format(plotter.out_dir, fig_name, time_data['write_number'][ni]), dpi=200, bbox_inches='tight')
-#        for pax in [cax, line_ax1, line_ax2]:
-        for pax in [cax, ]:
-           pax.clear()
+
+        gc.collect()
+        ax.clear()
+        first = False
