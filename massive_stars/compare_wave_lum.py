b870cff6bdbfee29c4eb1bdc1020dc0a1abe4920
Evan Anders <evanhanders@gmail.com>
2023-02-02 09:52:20 -0600

    makes the wave luminosity script a bit better

--- massive_stars/compare_wave_lum.py
+++ massive_stars/compare_wave_lum.py
@@ -2,21 +2,34 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
+rv = '1.25'
+
+with h5py.File('twoRcore_re2e4_damping/wave_flux/wave_luminosities.h5', 'r') as f:
+    freqs_384 = f['cgs_freqs'][()]
+    ells_384  = f['ells'][()].ravel()
+#    power = f['vel_power(r=1.1)'][0,:]
+    lum_384 = f['cgs_wave_luminosity(r={})'.format(rv)][0,:]
+
 with h5py.File('twoRcore_re1e4_damping/wave_flux/wave_luminosities.h5', 'r') as f:
     freqs_256 = f['cgs_freqs'][()]
     ells_256  = f['ells'][()].ravel()
 #    power = f['vel_power(r=1.1)'][0,:]
-    lum_256 = f['cgs_wave_luminosity(r=1.25)'][0,:]
+    lum_256 = f['cgs_wave_luminosity(r={})'.format(rv)][0,:]
+
+with h5py.File('twoRcore_re2e3_damping/wave_flux/wave_luminosities.h5', 'r') as f:
+    freqs_128_low = f['cgs_freqs'][()]
+    ells_128_low  = f['ells'][()].ravel()
+    lum_128_low = f['cgs_wave_luminosity(r={})'.format(rv)][0,:]
 
 with h5py.File('twoRcore_re4e3_damping/wave_flux/wave_luminosities.h5', 'r') as f:
     freqs_128 = f['cgs_freqs'][()]
     ells_128  = f['ells'][()].ravel()
-    lum_128 = f['cgs_wave_luminosity(r=1.25)'][0,:]
+    lum_128 = f['cgs_wave_luminosity(r={})'.format(rv)][0,:]
 
 with h5py.File('twoRcore_re1e3_damping/wave_flux/wave_luminosities.h5', 'r') as f:
     freqs_96 = f['cgs_freqs'][()]
     ells_96  = f['ells'][()].ravel()
-    lum_96 = f['cgs_wave_luminosity(r=1.25)'][0,:]
+    lum_96 = f['cgs_wave_luminosity(r={})'.format(rv)][0,:]
 
 
 
@@ -37,24 +50,34 @@ with h5py.File('twoRcore_re1e3_damping/wave_flux/wave_luminosities.h5', 'r') as 
 #plt.axvline(128)
 #plt.show()
 #
-#for freq in [0.2, 0.3, 0.4, 0.5]:
-#    plt.loglog(ells_128, lum_r1_128[freqs_128 > freq, :][0,:], label='128 (r=1.1)')
-#    plt.loglog(ells_128, lum_128[freqs_128 > freq, :][0,:], label='128 (r=1.25)')
-#    plt.loglog(ells_256, lum_r1_256[freqs_256 > freq, :][0,:], label='256 (r=1.1)')
-#    plt.loglog(ells_256, lum_256[freqs_256 > freq, :][0,:], label='256 (r=1.25)')
-#    plt.loglog(ells_256, 2.14e-28*freq**(-6.5)*ells_256**2, c='k')
-#    plt.ylabel('wave luminosity')
-#    plt.xlabel(r'$\ell$')
-#    plt.legend()
-#    plt.title(r'$f = $' + '{}'.format(freq))
-#    plt.show()
-#
-for ell in range(1, 10):
-    plt.loglog(freqs_96, lum_96[:, ells_96 == ell], label='96')
-    plt.loglog(freqs_128, lum_128[:, ells_128 == ell], label='128')
-    plt.loglog(freqs_256, lum_256[:, ells_256 == ell], label='256')
+
+from palettable.colorbrewer.sequential import RdPu_5
+for freq in [3e-6, 5e-6, 1e-5]:
+    print('f = {}'.format(freq))
+    plt.loglog(ells_96, lum_96[freqs_96 > freq, :][0,:],                 color=RdPu_5.mpl_colors[0], label=r'Re $\sim$ 200')
+    plt.loglog(ells_128_low, lum_128_low[freqs_128_low > freq, :][0,:],  color=RdPu_5.mpl_colors[1], label=r'Re $\sim$ 400')
+    plt.loglog(ells_128, lum_128[freqs_128 > freq, :][0,:],              color=RdPu_5.mpl_colors[2], label=r'Re $\sim$ 800')
+    plt.loglog(ells_256, lum_256[freqs_256 > freq, :][0,:],              color=RdPu_5.mpl_colors[3], label=r'Re $\sim$ 2000')
+    plt.loglog(ells_384, lum_384[freqs_384 > freq, :][0,:],              color=RdPu_5.mpl_colors[4], label=r'Re $\sim$ 4000')
+    kh = np.sqrt(ells_384*(ells_384+1))
+    plt.loglog(ells_384, 3e-11*freq**(-6.5)*kh**4, c='k', label=r'$(3\times10^{-11})f^{-6.5}\left[\sqrt{\ell(\ell+1)}\,\right]^{4}$')
+    plt.ylabel('wave luminosity')
+    plt.xlabel(r'$\ell$')
+    plt.legend()
+    plt.title(r'$f = $' + '{}'.format(freq))
+    plt.savefig('wave_luminosity_comparison/freq{:0.2e}.png'.format(freq))
+    plt.clf()
+
+for ell in range(1, 4):
+    print('ell = {}'.format(ell))
+    plt.loglog(freqs_96,      np.abs(lum_96[:, ells_96 == ell]),           color=RdPu_5.mpl_colors[0], label=r'Re $\sim$ 200')
+    plt.loglog(freqs_128_low, np.abs(lum_128_low[:, ells_128_low == ell]), color=RdPu_5.mpl_colors[1], label=r'Re $\sim$ 400')
+    plt.loglog(freqs_128,     np.abs(lum_128[:, ells_128 == ell]),         color=RdPu_5.mpl_colors[2], label=r'Re $\sim$ 800')
+    plt.loglog(freqs_256,     np.abs(lum_256[:, ells_256 == ell]),         color=RdPu_5.mpl_colors[3], label=r'Re $\sim$ 2000')
+    plt.loglog(freqs_384,     np.abs(lum_384[:, ells_384 == ell]),         color=RdPu_5.mpl_colors[4], label=r'Re $\sim$ 4000')
 #    plt.loglog(freqs_256, 2.14e-28*freqs_256**(-6.5)*ell**2, c='k')
-    plt.loglog(freqs_256, 1e-15*freqs_256**(-7.5)*ell**3, c='k')
+    kh = np.sqrt(ell*(ell+1))
+    plt.loglog(freqs_256, 3e-11*freqs_256**(-6.5)*kh**4, c='k', label=r'$(3\times10^{-11})f^{-6.5}\left[\sqrt{\ell(\ell+1)}\,\right]^{4}$')
     plt.xlabel('freq')
     plt.ylabel('wave luminosity')
     plt.xlim(1e-6, 1e-4)
@@ -63,4 +86,6 @@ for ell in range(1, 10):
     plt.title(r'$\ell = $' + '{}'.format(ell))
     plt.axvline(0.2)
     plt.axvline(0.5)
-    plt.show()
+    plt.savefig('wave_luminosity_comparison/ell{:03d}.png'.format(ell))
+    plt.clf()
+#    plt.show()
