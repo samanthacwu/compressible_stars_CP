7fd90a79c06d17b516258afc0240339e7915ae29
Evan Anders <evanhanders@gmail.com>
2023-02-02 19:21:40 -0600

    adds re=4e3 msol = [3, 40] runs

---
+++ massive_stars/other_stars/msol40_twoRcore_re4e3_damping/post_transfer_power_spectrum.py
@@ -0,0 +1,203 @@
+"""
+This script computes the wave flux in a d3 spherical simulation
+
+Usage:
+    post_transfer_power_spectrum.py
+"""
+import re
+import gc
+import os
+import time
+import sys
+from collections import OrderedDict
+from pathlib import Path
+
+import h5py
+import numpy as np
+from docopt import docopt
+from configparser import ConfigParser
+from scipy import sparse
+
+from plotpal.file_reader import SingleTypeReader as SR
+import matplotlib.pyplot as plt
+
+import logging
+logger = logging.getLogger(__name__)
+
+from dedalus.tools.config import config
+from scipy.interpolate import interp1d
+
+from d3_stars.defaults import config
+from d3_stars.simulations.parser import name_star
+out_dir, out_file = name_star()
+
+fit_wave_flux = False
+
+#args = docopt(__doc__)
+with h5py.File(out_file, 'r') as f:
+    rB = f['r_B'][()]
+    rS1 = f['r_S1'][()]
+    rS2 = f['r_S2'][()]
+    rhoB = np.exp(f['ln_rho0_B'][()])
+    rhoS1 = np.exp(f['ln_rho0_S1'][()])
+    rhoS2 = np.exp(f['ln_rho0_S2'][()])
+    r = np.concatenate((rB.flatten(), rS1.flatten(), rS2.flatten()))
+    rho = np.concatenate((rhoB.flatten(), rhoS1.flatten(), rhoS2.flatten()))
+    rho_func = interp1d(r,rho)
+    tau_nd = f['tau_nd'][()]
+    #Entropy units are erg/K/g
+    N2plateau = f['N2plateau'][()] * tau_nd**2
+    N2mesa = f['N2_mesa'][()] * tau_nd**2
+    r_mesa = f['r_mesa'][()] / f['L_nd'][()]
+    N2_func = interp1d(r_mesa, N2mesa)
+    R_gas = f['R_gas'][()]
+    Cp = f['Cp'][()]
+    chi_rad_min = f['chi_rad_B'][()].min()
+
+full_out_dir = 'damping_theory_power'
+if not os.path.exists(full_out_dir):
+    os.makedirs(full_out_dir)
+
+if fit_wave_flux:
+    #Fit wave luminosity
+    #Fit A f ^ alpha ell ^ beta
+    radius_str = '1.1'
+    fit_freq_range = (2e-2, 1e-1)
+    fit_ell_range = (1, 4)
+    fig = plt.figure()
+    #possible_alphas = [-6, -13/2, -7]
+    possible_alphas = [-6.5]#[-5, -5.25, -11/2, -5.75, -6, -6.25, -13/2, -6.75, -7, -15/2, -8]
+    possible_betas = [3, 3.25, 3.5, 3.75, 4]
+    fit_A = []
+    fit_alpha = []
+    fit_beta  = []
+    with h5py.File('FT_SH_transform_wave_shells/wave_luminosities.h5', 'r') as lum_file:
+        freqs = lum_file['freqs'][()]
+        good_freqs = (freqs >= fit_freq_range[0])*(freqs <= fit_freq_range[1])
+        ells = lum_file['ells'][()].ravel()
+        good_ells = (ells >= fit_ell_range[0])*(ells <= fit_ell_range[1])
+        for i in range(lum_file['wave_luminosity(r={})'.format(radius_str)][()].shape[0]):
+            wave_luminosity = np.abs(lum_file['wave_luminosity(r={})'.format(radius_str)][i,:,:])
+            info = []
+            error = []
+            for j, alpha in enumerate(possible_alphas):
+                for k, beta in enumerate(possible_betas):
+                    A = np.mean((wave_luminosity / freqs[:,None]**(alpha) / ells[None,:]**(beta))[good_freqs[:,None]*good_ells[None,:]])
+                    fit = A * freqs[:,None]**alpha * ells[None,:]**beta
+                    error.append(np.mean( np.abs(1 - (np.log10(fit) / np.log10(wave_luminosity))[good_freqs[:,None]*good_ells[None,:]])))
+                    info.append((A, alpha, beta))
+            print(info, error)
+            A, alpha, beta = info[np.argmin(error)]
+
+            fit_A.append(A)
+            fit_alpha.append(alpha)
+            fit_beta.append(beta)
+    wave_luminosity_power = lambda f, ell: fit_A[-1]*f**(fit_alpha[-1])*ell**(fit_beta[-1])
+    print('fit_A', fit_A)
+    print('fit_A frac', np.array(fit_A[1:])/np.array(fit_A[:-1]))
+    print('fit_alpha', fit_alpha)
+    print('fit_beta', fit_beta)
+#    for ell in ells:
+#        plt.loglog(freqs, wave_luminosity[:,ell == ells].ravel())
+#        plt.loglog(freqs, wave_luminosity_power(freqs, ell))
+#        plt.show()
+
+else:
+    radius_str = '1.1'
+    wave_lums = dict()
+    with h5py.File('FT_SH_transform_wave_shells/wave_luminosities.h5', 'r') as lum_file:
+        wave_lums['freqs'] = lum_file['freqs'][()]
+        wave_lums['ells'] = lum_file['ells'][()].ravel()
+        wave_lums['lum'] = lum_file['wave_luminosity(r={})'.format(radius_str)][0,:]
+
+freqs = np.logspace(-3, 0, 1000)
+t_freqs = np.logspace(np.log10(freqs.min()), np.log10(freqs.max()), 100000)
+
+
+with h5py.File('../compressible_re4e3_waves/FT_SH_transform_wave_shells/power_spectra.h5', 'r') as pow_f:
+    surface_power = pow_f['shell(s1_S2,r=R)'][-1,:]
+    surface_ells = pow_f['ells'][()].squeeze()
+    surface_freqs = pow_f['freqs'][()].squeeze()
+    print('surface power shape', surface_power.shape)
+
+
+        
+powers = []
+ell_vals = []
+fig = plt.figure()
+for ell in range(64):
+    if ell == 0: continue
+    try:
+        print('plotting ell = {}'.format(ell))
+        with h5py.File('eigenvalues/duals_ell{:03d}_eigenvalues.h5'.format(ell), 'r') as f:
+            smooth_oms = f['smooth_oms'][()]
+            smooth_depths = f['smooth_depths'][()]
+            depthfunc = interp1d(smooth_oms/(2*np.pi), smooth_depths, bounds_error=False, fill_value='extrapolate')
+
+        with h5py.File('eigenvalues/transfer_ell{:03d}_eigenvalues.h5'.format(ell), 'r') as ef:
+            transfer_func = ef['transfer'][()]
+            transfer_freq = ef['om'][()]/(2*np.pi)
+            transfer_interp = lambda f: 10**interp1d(np.log10(transfer_freq), np.log10(transfer_func), bounds_error=False, fill_value=np.nan)(np.log10(f))
+
+        if fit_wave_flux:
+            wave_flux_rcb = lambda f: wave_luminosity_power(f,ell)/(4*np.pi*1**2*rho_func(1))
+        else:
+            log_wave_flux = interp1d(np.log10(wave_lums['freqs']), np.log10(wave_lums['lum'][:,ell == wave_lums['ells']].ravel()/(4*np.pi*1**2*rho_func(1))))
+            wave_flux_rcb = lambda f: 10**(log_wave_flux(np.log10(f)))
+
+        #wave_lum_ell should be wave_flux_ell? - see slack stuff around sept 29 2021
+        kh2 = ell * (ell + 1) / 1**2 #at r = 1
+        kr = lambda f: np.abs(( ((-1)**(3/4) / np.sqrt(2))\
+                          *np.sqrt(-2*1j*kh2 - (2*np.pi*f/chi_rad_min) + np.sqrt((2*np.pi*f)**3 + 4*1j*kh2*chi_rad_min*N2plateau)/(chi_rad_min*np.sqrt(2*np.pi*f)) )).real)
+        ur2 = lambda f: (2*np.pi*f) * (R_gas / Cp) * np.sqrt(kr(f)**2) * wave_flux_rcb(f) / N2plateau 
+#        plt.loglog(t_freqs, kr(t_freqs)**2)
+#        plt.loglog(t_freqs, kh2*(N2plateau/(2*np.pi*t_freqs)**2 - 1))
+#        plt.axvline(N2plateau**(1/2)/(2*np.pi))
+#        plt.show()
+        fudge =  1
+        surface_s1_power = lambda f: fudge * np.conj(transfer_interp(f))*transfer_interp(f) * ur2(f)
+
+
+
+        powers.append(surface_s1_power(t_freqs))
+        plt.loglog(surface_freqs, surface_power[:,ell], c='orange', label='sim', lw=1.5)
+#        plt.loglog(transfer_freq, np.exp(-depthfunc(transfer_freq))*np.conj(transfer_func)*transfer_func*ur2(transfer_freq), c='green', label='transfer', lw=0.5)
+        plt.loglog(t_freqs, powers[-1], c='k',  label='transfer', lw=1)
+        plt.legend(loc='best')
+        plt.title('ell={}'.format(ell))
+        plt.xlabel('freqs (sim units)')
+        plt.ylabel(r'power')
+        plt.ylim(1e-30, 1e-10)
+        plt.xlim(3e-3, 1.4)
+        fig.savefig('{}/s1_simulated_freq_spectrum_ell{}.png'.format(full_out_dir, ell), dpi=300, bbox_inches='tight')
+        plt.clf()
+        ell_vals.append(ell)
+    except:
+#        raise
+        print("no eigenvalues for ell = {}".format(ell))
+        
+ell_vals = np.array(ell_vals)
+powers = np.array(powers)
+print(powers.shape)
+sim_sum_power = np.sum(surface_power[:,1:ell_vals[-1]+1], axis=1)
+sim_sum_hemisphere_power = np.sum((surface_power/surface_ells)[:,1:ell_vals[-1]+1], axis=1)
+sum_ells_power = np.sum(powers, axis=0)
+sum_ells_hemisphere_power = np.sum(powers/ell_vals[:,None], axis=0)
+plt.loglog(surface_freqs, sim_sum_power, c='orange', lw=1)
+plt.loglog(surface_freqs, sim_sum_hemisphere_power, c='orange', lw=0.5)
+plt.loglog(t_freqs, sum_ells_power, c='k', label=r'$\sum_{\ell} P_\ell$', lw=1)
+plt.loglog(t_freqs, sum_ells_hemisphere_power, c='k', label=r'$\sum_{\ell} \frac{P_\ell}{\ell}$', lw=0.5)
+plt.legend(loc='best')
+plt.title('summed over ells')
+plt.xlabel('freqs (sim units)')
+plt.ylabel(r'power')
+plt.ylim(1e-30, 1e-10)
+plt.xlim(3e-3, 1.4)
+fig.savefig('{}/s1_simulated_freq_spectrum_summed_ells.png'.format(full_out_dir), dpi=300, bbox_inches='tight')
+fig.savefig('{}/s1_simulated_freq_spectrum_summed_ells.pdf'.format(full_out_dir), dpi=300, bbox_inches='tight')
+
+
+with h5py.File('{}/simulated_powers.h5'.format(full_out_dir), 'w') as f:
+    f['powers'] = powers
+    f['freqs'] = freqs
+    f['sum_powers'] = sum_ells_power
---
+++ massive_stars/other_stars/msol40_twoRcore_re4e3_damping/transfer.py
@@ -0,0 +1,174 @@
+"""
+Calculate transfer function to get surface response of convective forcing.
+Outputs a function which, when multiplied by sqrt(wave flux), gives you the surface response.
+"""
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+from scipy import interpolate
+from pathlib import Path
+from scipy.interpolate import interp1d
+from configparser import ConfigParser
+
+import d3_stars
+from d3_stars.defaults import config
+from d3_stars.simulations.parser import name_star
+from d3_stars.simulations.star_builder import find_core_cz_radius
+from d3_stars.simulations.evp_functions import calculate_refined_transfer
+
+#Grab relevant information about the simulation stratification.
+out_dir, out_file = name_star()
+with h5py.File(out_file, 'r') as f:
+    L_nd = f['L_nd'][()]
+    rs = []
+    rhos = []
+    chi_rads = []
+    N2s = []
+    for bk in ['B', 'S1', 'S2']:
+        rs.append(f['r_{}'.format(bk)][()])
+        rhos.append(np.exp(f['ln_rho0_{}'.format(bk)][()]))
+        chi_rads.append(f['chi_rad_{}'.format(bk)][()])
+        #N^2 = -g[2] * grad_s[2] / cp
+        N2s.append(-f['g_{}'.format(bk)][2,:]*f['grad_s0_{}'.format(bk)][2,:]/f['Cp'][()])
+    rs = np.concatenate(rs, axis=-1)
+    rhos = np.concatenate(rhos, axis=-1)
+    chi_rads = np.concatenate(chi_rads, axis=-1)
+    N2s = np.concatenate(N2s, axis=-1)
+rho = interpolate.interp1d(rs.flatten(), rhos.flatten())
+chi_rad = interpolate.interp1d(rs.flatten(), chi_rads.flatten())
+N2 = interpolate.interp1d(rs.flatten(), N2s.flatten())
+
+
+# Generalized logic for getting forcing radius.
+package_path = Path(d3_stars.__file__).resolve().parent
+stock_path = package_path.joinpath('stock_models')
+if os.path.exists(config.star['path']):
+    mesa_file_path = config.star['path']
+else:
+    stock_file_path = stock_path.joinpath(config.star['path'])
+    if os.path.exists(stock_file_path):
+        mesa_file_path = str(stock_file_path)
+    else:
+        raise ValueError("Cannot find MESA profile file in {} or {}".format(config.star['path'], stock_file_path))
+core_cz_radius = find_core_cz_radius(mesa_file_path)
+forcing_radius = 1.02 * core_cz_radius / L_nd
+
+
+#Calculate transfer functions
+Lmax = config.eigenvalue['Lmax']
+ell_list = np.arange(1, Lmax+1)
+eig_dir = 'eigenvalues'
+for ell in ell_list:
+    plt.figure()
+    print("ell = %i" % ell)
+
+    xmin = 1e99
+    xmax = -1e99
+
+    transfers = []
+    oms = []
+    depth_list = [10, 1, 0.01]
+    for j, d_filter in enumerate(depth_list):
+        #Read in eigenfunction values.
+        #Require: eigenvalues, horizontal duals, transfer surface (s1), optical depths
+
+        with h5py.File('{:s}/duals_ell{:03d}_eigenvalues.h5'.format(eig_dir, ell), 'r') as f:
+            velocity_duals = f['velocity_duals'][()]
+            values = f['good_evalues'][()]
+            s1_amplitudes = f['s1_amplitudes'][()].squeeze()
+            depths = f['depths'][()]
+
+            rs = []
+            for bk in ['B', 'S1', 'S2']:
+                rs.append(f['r_{}'.format(bk)][()].flatten())
+            r = np.concatenate(rs)
+            smooth_oms = f['smooth_oms'][()]
+            smooth_depths = f['smooth_depths'][()]
+            depthfunc = interp1d(smooth_oms, smooth_depths, bounds_error=False, fill_value='extrapolate')
+
+        #Pick out eigenvalues that have less optical depth than a given cutoff
+        print('depth cutoff: {}'.format(d_filter))
+        good = depths < d_filter
+        values = values[good]
+        s1_amplitudes = s1_amplitudes[good]
+        velocity_duals = velocity_duals[good]
+
+        #Construct frequency grid for evaluation
+        om0 = np.min(np.abs(values.real))
+        om1 = np.max(values.real)*5
+        if om0 < xmin: xmin = om0
+        if om1 > xmax: xmax = om1
+        if j == 0:
+            om0/= 10**(1)
+            stitch_om = np.abs(values[depths[good] <= 1][-1].real)
+        om = np.exp( np.linspace(np.log(om0), np.log(om1), num=5000, endpoint=True) )
+
+        #Get forcing radius and dual basis evaluated there.
+        r0 = forcing_radius
+        r1 = forcing_radius * (1.05)
+        r_range = np.linspace(r0, r1, num=100, endpoint=True)
+        uphi_dual_interp = interpolate.interp1d(r, velocity_duals[:,0,:], axis=-1)(r_range)
+
+        #Calculate and store transfer function
+        om, T = calculate_refined_transfer(om, values, uphi_dual_interp, s1_amplitudes, r_range, rho(r_range), ell)
+        oms.append(om)
+        transfers.append(T)
+
+
+    #right now we have a transfer function for each optical depth filter
+    # We want to just get one transfer function value at each om
+    # use the minimum T value from all filters we've done.
+    good_om = np.sort(np.concatenate(oms))
+    good_T = np.zeros_like(good_om)
+
+    from scipy.interpolate import interp1d
+    interps = []
+    for om, T in zip(oms, transfers):
+        interps.append(interp1d(om, T, bounds_error=False, fill_value=np.inf))
+
+    for i, omega in enumerate(good_om):
+        vals = []
+        for f in interps:
+            vals.append(f(omega))
+        good_T[i] = np.min(vals)
+
+    #Do WKB at low frequency -- exponentially attenuate by exp(-om/om_{tau=1}) [assume transfer does everything right up to tau=1]
+#    good_T *= np.exp(-depthfunc(good_om))
+    good_T[good_om <= stitch_om] *= np.exp(-depthfunc(good_om[good_om <= stitch_om]) + depthfunc(stitch_om))
+
+
+    # Right now the transfer function gets us from ur (near RCB) -> surface. We want wave luminosity (near RCB) -> surface.
+    chi_rad_u = chi_rad(forcing_radius)
+    brunt_N2_u = N2(forcing_radius)
+    rho_u = rho(forcing_radius)
+
+    #k_r is eqn 13 of Lecoanet+2015 PRE 91, 063016.
+    k_h = np.sqrt(ell*(ell+1))/forcing_radius 
+    k_r_low_diss  = -np.sqrt(brunt_N2_u/good_om**2 - 1).real*k_h
+    k_r_high_diss = (((-1)**(3/4) / np.sqrt(2))\
+                          *np.sqrt(-2*1j*k_h**2 - (good_om/chi_rad_u) + np.sqrt((good_om)**3 + 4*1j*k_h**2*chi_rad_u*brunt_N2_u)/(chi_rad_u*np.sqrt(good_om)) )).real
+    k_r_err = np.abs(1 - k_r_low_diss/k_r_high_diss)
+
+    k_r = np.copy(k_r_high_diss)
+    if np.sum(k_r_err < 1e-3) > 0:
+        om_switch = good_om.ravel()[k_r_err < 1e-3][0]
+        k_r[good_om.ravel() >= om_switch] = k_r_low_diss[good_om.ravel() >= om_switch]
+#
+    k2 = k_r**2 + k_h**2
+    root_lum_to_ur = np.sqrt(1/(4*np.pi*forcing_radius**2*rho_u))*np.sqrt(np.array(1/(-(good_om + 1j*chi_rad_u*k2)*k_r / k_h**2).real, dtype=np.complex128))
+
+#    plt.loglog(good_om/(2*np.pi), good_T)
+#    plt.loglog(good_om/(2*np.pi), (root_lum_to_ur*good_T).real, color='orange')
+#    plt.loglog(good_om/(2*np.pi), (root_lum_to_ur*good_T).imag, color='orange', ls='--')
+#    plt.axvline(stitch_om/(2*np.pi))
+#    plt.xlabel('frequency')
+#    plt.ylabel('T')
+#    plt.show()
+#
+    with h5py.File('{:s}/transfer_ell{:03d}_eigenvalues.h5'.format(eig_dir, ell), 'w') as f:
+        f['om'] = good_om
+        f['transfer_ur'] = good_T
+        f['transfer_root_lum'] = root_lum_to_ur*good_T 
+
+
