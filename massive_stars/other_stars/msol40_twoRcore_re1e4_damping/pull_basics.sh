86e7fc346917431a4a9cb561c115caaa879ccc96
Evan Anders <evanhanders@gmail.com>
2023-02-03 11:09:12 -0600

    makes new msol=3 and msol=40 directories

---
+++ massive_stars/other_stars/msol40_twoRcore_re1e4_damping/pull_basics.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pleiades_dir=/nobackup/eanders/d3_stars/massive_stars/other_stars/msol40_twoRcore_re4e3_damping/
+local_dir=basics
+files=(snapshots_equatorial.mp4,flux_plots.mp4)
+mkdir $local_dir
+for sf in energy_fluc mass
+do
+    files=(${files[@]},traces/$sf\.png)
+done
+
+files={${files[@]}}
+echo $files
+scp pfe:$pleiades_dir/$files ./$local_dir/
+
