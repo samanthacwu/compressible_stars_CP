c91481c9d63cc619bb6c5748d9e3f7fda869a3bd
Evan Anders <evanhanders@gmail.com>
2023-02-25 12:56:32 -0600

    updates fig 2 script

--- massive_stars/gyre/plot_surface_luminosity.py
+++ massive_stars/gyre/plot_surface_luminosity.py
@@ -19,6 +19,11 @@ from d3_stars.simulations.parser import name_star
 from d3_stars.simulations.star_builder import find_core_cz_radius
 from d3_stars.simulations.evp_functions import calculate_refined_transfer
 from matplotlib.patches import ConnectionPatch
+from palettable.colorbrewer.qualitative import Dark2_5 as cmap
+plt.rcParams['font.family'] = ['Times New Roman']
+plt.rcParams['mathtext.fontset'] = 'dejavusans'
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['mathtext.rm'] = 'serif'
 
 
 #Appendix information from https://www.aanda.org/articles/aa/pdf/2020/08/aa38224-20.pdf
@@ -30,9 +35,6 @@ Cw           = [3.814, 9.116, 3.770, 1.445, 3.490, 2.889] #mumag
 log10Teff    = [4.46, 4.47, 4.41, 4.46, 4.40, 4.38]
 log10LdLsol  = [3.31, 3.06, 2.95, 3.22, 3.28, 3.31]
 
-Bdwarf_star_names   = star_names[:3]
-Bsubgiant_star_names   = star_names[3:]
-
 #NOTE to compare to MESA model we need to calculate 'ell' not bolometric luminosity.
 # from matteo:
 #ell_sun=(5777)**4.0/(274*100)  
@@ -40,32 +42,23 @@ Bsubgiant_star_names   = star_names[3:]
 #ell=np.log10(ell/ell_sun)  
 sLum_sun=(5777)**4.0/(274*100)  
 
+output_file = 'magnitude_spectra.h5'
+out_f = h5py.File(output_file, 'r')
+freqs = out_f['frequencies'][()]
+
 
 #Calculate transfer functions
-Lmax = 16
-ell_list = np.arange(1, Lmax+1)
 eig_dir = 'gyre_output'
 
 
 plt.figure()
 star_dirs = ['3msol', '40msol', '15msol']
-luminosity_amplitudes = [3e-15, 3e-10, 3e-11]
-#star_dirs = ['15msol', '15msol', '40msol']
-#luminosity_amplitudes = [3e-11, 3e-11, 3e-10]
-obs_length_days = 365
-obs_length_sec  = obs_length_days*24*60*60
-obs_cadence = 30*60 #30 min
-df = 1/obs_length_sec
-N_data = int(obs_length_sec/obs_cadence)
-freqs = np.arange(N_data)*df
+Lmax = [16,16,16]
 
 signals = []
 specLums = []
 logTeffs = []
 for i, sdir in enumerate(star_dirs):
-    wave_luminosity = lambda f, l: luminosity_amplitudes[i]*f**(-6.5)*np.sqrt(l*(l+1))**4
-    transfer_oms = []
-    transfer_signal = []
 
     #MESA history for getting ell.
     mesa_history = '{}/LOGS/history.data'.format(sdir)
@@ -80,39 +73,8 @@ for i, sdir in enumerate(star_dirs):
     logTeffs.append(log_Teff[-1])
 
 
-    plt.figure()
-    for ell in ell_list:
-        print("ell = %i" % ell)
 
-
-        with h5py.File('{:s}/{:s}/ell{:03d}_eigenvalues.h5'.format(sdir, eig_dir, ell), 'r') as f:
-            om = f['transfer_om'][()]
-            transfer_root_lum = f['transfer_root_lum'][()].real
-        micromag = transfer_root_lum*np.sqrt(wave_luminosity(om/(2*np.pi), ell))
-
-        plt.loglog(om/(2*np.pi), micromag, label='ell={}'.format(ell))
-        plt.ylabel(r'$\delta L / L_*$')
-        plt.xlabel(r'frequency (Hz)')
-        plt.xlim(3e-7, 1e-4)
-
-        transfer_oms.append(om[np.isfinite(micromag)])
-        transfer_signal.append(micromag[np.isfinite(micromag)])
-
-
-
-    total_signal = np.zeros_like(freqs)
-    for i in range(freqs.size-1):
-        for oms, signal in zip(transfer_oms, transfer_signal):
-            good = (2*np.pi*freqs[i+1] >= oms)*(2*np.pi*freqs[i] < oms)
-            if np.sum(good) > 0:
-                total_signal[i] += np.max(signal[good])
-            elif 2*np.pi*freqs[i] > oms.min():
-                total_signal[i] += signal[np.argmin(np.abs(2*np.pi*freqs[i] - oms))]
-    plt.loglog(freqs, total_signal, c='k')
-    plt.legend()
-    plt.savefig('obs_ell_contributions_{}.png'.format(sdir), bbox_inches='tight')
-    signals.append(total_signal)
-
+    signals.append(np.sum(out_f['{}_magnitude_cube'.format(sdir)][:Lmax[i],:], axis=0))
 freqs *= 60*60*24 #1/s -> 1/day
 
 #### MAKE PAPER FIGURE
@@ -120,10 +82,10 @@ fig = plt.figure(figsize=(8,3))
 ax1 = fig.add_axes((0,    0, 0.27, 1))
 ax2 = fig.add_axes((0.40, 0, 0.27, 1))
 ax3 = fig.add_axes((0.73,  0, 0.27, 1))
-con1 = ConnectionPatch(xyA=(1e1,1e0), xyB=(3e-2,1e0), coordsA='data', coordsB='data', axesA=ax2, axesB=ax3, color='k', lw=0.5)
-con2 = ConnectionPatch(xyA=(1e1,1e-4), xyB=(3e-2,1e-4), coordsA='data', coordsB='data', axesA=ax2, axesB=ax3, color='k', lw=0.5)
-ax2.add_artist(con1)
-ax2.add_artist(con2)
+#con1 = ConnectionPatch(xyA=(1e1,1e0), xyB=(3e-2,1e0), coordsA='data', coordsB='data', axesA=ax2, axesB=ax3, color='k', lw=0.5)
+#con2 = ConnectionPatch(xyA=(1e1,1e-4), xyB=(3e-2,1e-4), coordsA='data', coordsB='data', axesA=ax2, axesB=ax3, color='k', lw=0.5)
+#ax2.add_artist(con1)
+#ax2.add_artist(con2)
 
 plt.subplots_adjust(hspace=0.5, wspace=0.7)
 
@@ -131,6 +93,8 @@ plt.subplots_adjust(hspace=0.5, wspace=0.7)
 #Reads in non-rotating mist models.
 #Data: https://waps.cfa.harvard.edu/MIST/model_grids.html (v/vcrit = 0; [Fe/H] = 0) EEP tracks.
 #Read script: https://github.com/jieunchoi/MIST_codes/blob/master/scripts/read_mist_models.py
+zamsT = []
+zamsL = []
 for mass in ['00300', '00500', '01000', '02000', '04000']:
     model = EEP('mist/MIST_v1.2_feh_p0.00_afe_p0.0_vvcrit0.0_EEPS/{}M.track.eep'.format(mass), verbose=True)
     mass = model.minit
@@ -140,16 +104,43 @@ for mass in ['00300', '00500', '01000', '02000', '04000']:
     log_g = model.eeps['log_g']
     ell = (10**log_Teff)**4.0/(10**log_g)
     ell = np.log10(ell/sLum_sun)
+    zamsT.append(log_Teff[good][0])
+    zamsL.append(ell[good][0])
     ax1.plot(log_Teff[good], ell[good], label=mass, c='grey', lw=1, zorder=0)
 #    ax1.text(0.01+log_Teff[good][0], -0.1+ell[good][0], '{:d}'.format(int(mass))+r'$M_{\odot}$', ha='right')
     ax1.text(0.02+log_Teff[good][0], -0.1+ell[good][0], '{:d}'.format(int(mass)), ha='right', size=10, color='grey')
 
-ax1.scatter(4.715, 1.4, c='white', marker='*', s=100, zorder=1, edgecolors='k', linewidths=0.5)
-ax1.text(4.715 - 0.04, 1.4, 'simulations', c='k', ha='left', va='center')
+#make colormap based on main sequence distance for stars
+#ax1.plot(zamsT, zamsL, c='grey', zorder=0, lw=1)
+denseT = np.linspace(4.2, 4.7, 200)
+denseL = interp1d(zamsT, zamsL, bounds_error=False, fill_value='extrapolate')(denseT)
+distance = []
+for T, L in zip(log10Teff, log10LdLsol):
+    distance.append(np.min(np.sqrt((denseT - T)**2 + (denseL - L)**2)))
+abs_star_distance = np.array(100*np.array(distance)/np.max(distance) - 1, dtype=int)
+black = (0, 0, 0)
+pink_rgbs = []
+for i in range(3):
+    pink_rgbs.append(np.linspace(cmap.mpl_colors[3][i], black[i], 150)[:100])
+star_colors = []
+for i in range(len(star_names)):
+    ind = abs_star_distance[i]
+    color = []
+    for j in range(3):
+        color.append(pink_rgbs[j][ind])
+    star_colors.append(color)
+
+#make legend & set clims, etc.
+ax1.scatter(0.05, 0.04, c='white', marker='*', s=100, zorder=1, edgecolors='k', linewidths=0.5, transform=ax1.transAxes)
+ax1.scatter(0.05, 0.09, c='white', marker='o', s=20, zorder=1, edgecolors='k',  linewidths=0.5, transform=ax1.transAxes)
+ax1.text(0.10, 0.0375, 'Simulations', c='k', ha='left', va='center', transform=ax1.transAxes)
+ax1.text(0.10, 0.09, 'Observed stars', ha='left', va='center', transform=ax1.transAxes, color='k')#cmap.mpl_colors[3])
 ax1.set_xlim(4.75, 4.0)
 ax1.set_ylim(1.3, 4.0)
-ax1.set_ylabel(r'$\mathrm{log}_{10}(\mathcal{L}/\mathcal{L}_{\odot})$')
-ax1.set_xlabel(r'$\mathrm{log}_{10}(T_{\rm eff})$')
+ax1.set_ylabel(r'$\log_{10}\, \mathscr{L} / \mathscr{L}_\odot$')
+ax1.set_xlabel(r'$\log_{10}\, $T$_{\rm eff}/$K')
+#ax1.set_ylabel(r'$\mathrm{log}_{10}(\mathcal{L}/\mathcal{L}_{\odot})$')
+#ax1.set_xlabel(r'$\mathrm{log}_{10}(T_{\rm eff})$')
 
 plt.axes(ax2)
 plt.fill_between([3e-2, 1e-1], 1e-20, 1e10, color='grey', alpha=0.5)
@@ -157,16 +148,13 @@ plt.fill_between([1e1, 3e1], 1e-20, 1e10, color='grey', alpha=0.5)
 ax2.text(0.12, 0.1, 'Predicted wave signal', ha='left')
 ax2.text(0.2, 2e2, 'Observed red noise', ha='left', va='center')
 
-from palettable.colorbrewer.qualitative import Dark2_5 as cmap
 for i in range(len(star_names)):
-    if star_names[i] in Bdwarf_star_names:
-        color = cmap.mpl_colors[3]
-        if star_names[i] == Bdwarf_star_names[0]:
-            ax1.text(0.99, 0.99, 'B dwarf stars', ha='right', va='top', transform=ax1.transAxes, color=color)
-    elif star_names[i] in Bsubgiant_star_names:
-        color = cmap.mpl_colors[4]
-        if star_names[i] == Bsubgiant_star_names[0]:
-            ax1.text(0.99, 0.95, 'B subgiant stars', ha='right', va='top', transform=ax1.transAxes, color=color)
+#    if star_names[i] in Bdwarf_star_names:
+    color = star_colors[i]
+#    elif star_names[i] in Bsubgiant_star_names:
+#        color = cmap.mpl_colors[4]
+#        if star_names[i] == Bsubgiant_star_names[0]:
+#            ax1.text(0.99, 0.95, 'B subgiant stars', ha='right', va='top', transform=ax1.transAxes, color=color)
     ax1.scatter(log10Teff[i], log10LdLsol[i], color=color, zorder=1, s=20, edgecolors='k', linewidths=0.5)
     alphanu = (alpha0[i] / (1 + (freqs/nu_char[i])**gamma[i]) + Cw[i]) #mags
     plt.loglog(freqs, alphanu, color=color)
