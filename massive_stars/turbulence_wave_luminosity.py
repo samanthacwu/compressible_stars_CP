488f991e12817765307697a75269d0f8637d1c83
Evan Anders <evanhanders@gmail.com>
2023-03-03 15:48:25 -0600

    updates wave luminosity plot scripts

--- massive_stars/turbulence_wave_luminosity.py
+++ massive_stars/turbulence_wave_luminosity.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import mesa_reader as mr
+plt.rcParams['font.family'] = ['Times New Roman']
+plt.rcParams['mathtext.fontset'] = 'dejavusans'
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['mathtext.rm'] = 'serif'
+
+
 
 mesa_LOG = '../d3_stars/stock_models/zams_15Msol/LOGS/profile47.data'
 p = mr.MesaData(mesa_LOG)
@@ -117,7 +123,7 @@ listed_cmap = mpl.colors.ListedColormap(cmap)
 sm = mpl.cm.ScalarMappable(norm=norm, cmap=listed_cmap)
 
 ell = 1
-freq = 0.5
+freq = 0.8
 ax1.loglog(freqs_96,      np.abs(lum_96[:, ells_96 == ell]),           color=cmap[0], label=r'Re $\sim$ 200',  zorder=1, lw=0.5)
 ax1.loglog(freqs_128_low, np.abs(lum_128_low[:, ells_128_low == ell]), color=cmap[1], label=r'Re $\sim$ 400',  zorder=2, lw=0.5)
 ax1.loglog(freqs_128,     np.abs(lum_128[:, ells_128 == ell]),         color=cmap[2], label=r'Re $\sim$ 800',  zorder=3, lw=0.5)
@@ -128,9 +134,9 @@ kh = np.sqrt(ell*(ell+1))
 ax1.loglog(freqs_256, (1/Lstar)*3e-11*(freqs_256/hz_to_invday)**(-6.5)*kh**4, c='k', label=r'$(3\times10^{-11})(f/Hz)^{-6.5}\left[\sqrt{\ell(\ell+1)}\,\right]^{4}$', zorder=7)
 ax1.set_ylim(1e-29, 1e-8)
 ax1.set_xlim(1e-2, 2e1)
-ax1.text(0.12, 3e28/Lstar, r'$f^{-6.5}$', rotation=0)
-ax1.set_xlabel(r'frequency (day$^{-1}$)')
-ax1.set_ylabel(r'$L_{\rm wave}/L_{*}$')
+ax1.text(0.15, 2e28/Lstar, r'$f^{-6.5}$', rotation=0)
+ax1.set_xlabel(r'$f$ (d$^{-1}$)')
+ax1.set_ylabel(r'$L_{w}/L_{*}$')
 #ax1.set_ylabel(r'$L_{\rm wave}/L_{*}$ ($L_{*} = 7.45 \times 10^{37}\,$ erg$\,$s$^{-1}$)')
 
 ax2.loglog(ells_96, lum_96[freqs_96 > freq, :][0,:],                 color=cmap[0], label=r'Re $\sim$ 200',  zorder=1, lw=1)
@@ -143,8 +149,9 @@ kh = np.sqrt(ells_384*(ells_384+1))
 ax2.loglog(ells_384, (1/Lstar)*3e-11*(freq/hz_to_invday)**(-6.5)*kh**4, c='k', label=r'$(3\times10^{-11})(f/Hz)^{-6.5}\left[\sqrt{\ell(\ell+1)}\,\right]^{4}$', zorder=7)
 ax2.set_xlim(1, 100)
 ax2.set_ylim(1e-29, 1e-8)
-ax2.text(25, 3e27/Lstar, r'$k_h^4=[\ell(\ell+1)]^2$', rotation=0,ha='left')
+ax2.text(15, 3e27/Lstar, r'$k_h^4=[\ell(\ell+1)]^2$', rotation=0,ha='right')
 ax2.set_xlabel(r'$\ell$')
+ax2.set_ylabel(r'$L_{w}/L_{*}$')
 
 for i, Re in enumerate([200, 400, 800, 2000, 4000, 6000]):
     if Re >= 1000:
@@ -156,6 +163,9 @@ for i, Re in enumerate([200, 400, 800, 2000, 4000, 6000]):
 cb = plt.colorbar(sm, cax=cax, orientation='horizontal')
 cax.text(-0.02, 0.5, 'Re', ha='right', va='center', transform=cax.transAxes)
 cb.set_ticks(())
+
+ax1.text(0.99, 0.98, r'$\ell = 1$', ha='right', va='top', transform=ax1.transAxes)
+ax2.text(0.01, 0.02, r'$f = 0.8$ (d$^{-1}$)', ha='left', va='bottom', transform=ax2.transAxes)
 
 plt.savefig('wave_luminosity_comparison/turbulence_waveflux_variation.png', dpi=300, bbox_inches='tight')
 plt.savefig('wave_luminosity_comparison/turbulence_waveflux_variation.pdf', dpi=300, bbox_inches='tight')
