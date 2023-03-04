eaf1154205e65c4ce9486cf0bd09d9a14f2b6b12
Evan Anders <evanhanders@gmail.com>
2023-03-04 15:27:08 -0600

    swaps order of plots in obs figure

--- massive_stars/gyre/plot_surface_luminosity.py
+++ massive_stars/gyre/plot_surface_luminosity.py
@@ -79,8 +79,8 @@ freqs *= 60*60*24 #1/s -> 1/day
 
 #### MAKE PAPER FIGURE
 fig = plt.figure(figsize=(8,3))
-ax1 = fig.add_axes((0,    0, 0.27, 1))
-ax2 = fig.add_axes((0.40, 0, 0.27, 1))
+ax1 = fig.add_axes((0.00,  0, 0.27, 1))
+ax2 = fig.add_axes((0.33,  0, 0.27, 1))
 ax3 = fig.add_axes((0.73,  0, 0.27, 1))
 
 plt.subplots_adjust(hspace=0.5, wspace=0.7)
@@ -102,12 +102,12 @@ for mass in ['00300', '00500', '01000', '02000', '04000']:
     ell = np.log10(ell/sLum_sun)
     zamsT.append(log_Teff[good][0])
     zamsL.append(ell[good][0])
-    ax1.plot(log_Teff[good], ell[good], label=mass, c='grey', lw=1, zorder=0)
-#    ax1.text(0.01+log_Teff[good][0], -0.1+ell[good][0], '{:d}'.format(int(mass))+r'$M_{\odot}$', ha='right')
-    ax1.text(0.02+log_Teff[good][0], -0.1+ell[good][0], '{:d}'.format(int(mass)), ha='right', size=10, color='grey')
+    ax3.plot(log_Teff[good], ell[good], label=mass, c='grey', lw=1, zorder=0)
+#    ax3.text(0.01+log_Teff[good][0], -0.1+ell[good][0], '{:d}'.format(int(mass))+r'$M_{\odot}$', ha='right')
+    ax3.text(0.02+log_Teff[good][0], -0.1+ell[good][0], '{:d}'.format(int(mass)), ha='right', size=10, color='grey')
 
 #make colormap based on main sequence distance for stars
-#ax1.plot(zamsT, zamsL, c='grey', zorder=0, lw=1)
+#ax3.plot(zamsT, zamsL, c='grey', zorder=0, lw=1)
 denseT = np.linspace(4.2, 4.7, 200)
 denseL = interp1d(zamsT, zamsL, bounds_error=False, fill_value='extrapolate')(denseT)
 distance = []
@@ -127,22 +127,22 @@ for i in range(len(star_names)):
     star_colors.append(color)
 
 #make legend & set clims, etc.
-ax1.scatter(0.05, 0.04, c='white', marker='*', s=100, zorder=1, edgecolors='k', linewidths=0.5, transform=ax1.transAxes)
-ax1.scatter(0.05, 0.09, c='white', marker='o', s=20, zorder=1, edgecolors='k',  linewidths=0.5, transform=ax1.transAxes)
-ax1.text(0.10, 0.0375, 'Simulations', c='k', ha='left', va='center', transform=ax1.transAxes)
-ax1.text(0.10, 0.09, 'Observed stars', ha='left', va='center', transform=ax1.transAxes, color='k')#cmap.mpl_colors[3])
-ax1.set_xlim(4.75, 4.0)
-ax1.set_ylim(1.3, 4.0)
-ax1.set_ylabel(r'$\log_{10}\, \mathscr{L} / \mathscr{L}_\odot$')
-ax1.set_xlabel(r'$\log_{10}\, $T$_{\rm eff}/$K')
-#ax1.set_ylabel(r'$\mathrm{log}_{10}(\mathcal{L}/\mathcal{L}_{\odot})$')
-#ax1.set_xlabel(r'$\mathrm{log}_{10}(T_{\rm eff})$')
+ax3.scatter(0.05, 0.04, c='white', marker='*', s=100, zorder=1, edgecolors='k', linewidths=0.5, transform=ax3.transAxes)
+ax3.scatter(0.05, 0.09, c='white', marker='o', s=20, zorder=1, edgecolors='k',  linewidths=0.5, transform=ax3.transAxes)
+ax3.text(0.10, 0.0375, 'Simulations', c='k', ha='left', va='center', transform=ax3.transAxes)
+ax3.text(0.10, 0.09, 'Observed stars', ha='left', va='center', transform=ax3.transAxes, color='k')#cmap.mpl_colors[3])
+ax3.set_xlim(4.75, 4.0)
+ax3.set_ylim(1.3, 4.0)
+ax3.set_ylabel(r'$\log_{10}\, \mathscr{L} / \mathscr{L}_\odot$')
+ax3.set_xlabel(r'$\log_{10}\, $T$_{\rm eff}/$K')
+#ax3.set_ylabel(r'$\mathrm{log}_{10}(\mathcal{L}/\mathcal{L}_{\odot})$')
+#ax3.set_xlabel(r'$\mathrm{log}_{10}(T_{\rm eff})$')
 
-plt.axes(ax2)
+plt.axes(ax1)
 #plt.fill_between([3e-2, 1e-1], 1e-20, 1e10, color='grey', alpha=0.5)
 #plt.fill_between([1e1, 3e1], 1e-20, 1e10, color='grey', alpha=0.5)
-ax2.text(0.12, 3e-2, 'Predicted wave signal', ha='left')
-ax2.text(0.2, 2e2, 'Observed red noise', ha='left', va='center')
+ax1.text(0.12, 3e-2, 'Predicted wave signal', ha='left')
+ax1.text(0.2, 2e2, 'Observed red noise', ha='left', va='center')
 
 for i in range(len(star_names)):
 #    if star_names[i] in Bdwarf_star_names:
@@ -150,12 +150,12 @@ for i in range(len(star_names)):
 #    elif star_names[i] in Bsubgiant_star_names:
 #        color = cmap.mpl_colors[4]
 #        if star_names[i] == Bsubgiant_star_names[0]:
-#            ax1.text(0.99, 0.95, 'B subgiant stars', ha='right', va='top', transform=ax1.transAxes, color=color)
-    ax1.scatter(log10Teff[i], log10LdLsol[i], color=color, zorder=1, s=20, edgecolors='k', linewidths=0.5)
+#            ax3.text(0.99, 0.95, 'B subgiant stars', ha='right', va='top', transform=ax3.transAxes, color=color)
+    ax3.scatter(log10Teff[i], log10LdLsol[i], color=color, zorder=1, s=20, edgecolors='k', linewidths=0.5)
     alphanu = (alpha0[i] / (1 + (freqs/nu_char[i])**gamma[i]) + Cw[i]) #mags
     plt.loglog(freqs, alphanu, color=color)
-#    ax1.text(log10Teff[i]*0.995, log10LdLsol[i], star_names[i], ha='left', color=color)
-#    ax1.text(0.99, 1.03-0.04*(i+1), star_names[i], ha='right', va='top', transform=ax1.transAxes, color=color)
+#    ax3.text(log10Teff[i]*0.995, log10LdLsol[i], star_names[i], ha='left', color=color)
+#    ax3.text(0.99, 1.03-0.04*(i+1), star_names[i], ha='right', va='top', transform=ax3.transAxes, color=color)
     plt.ylim(1e-4, 3e2)
     plt.ylabel(r'$\Delta m$ ($\mu$mag)')
     plt.xlabel(r'frequency (d$^{-1}$)')
@@ -163,25 +163,25 @@ for i in range(len(star_names)):
 min_plot_freq = 1e-3
 for i in range(3):
     star_log_Teff, star_log_Ell  = logTeffs[i], specLums[i]
-    ax1.scatter(star_log_Teff, star_log_Ell, c=cmap.mpl_colors[i], marker='*', s=100, zorder=1, edgecolors='k', linewidths=0.5)
+    ax3.scatter(star_log_Teff, star_log_Ell, c=cmap.mpl_colors[i], marker='*', s=100, zorder=1, edgecolors='k', linewidths=0.5)
     good = freqs >= min_plot_freq
-    ax3.loglog(freqs[good], signals[i][good], color=cmap.mpl_colors[i], lw=1)#, label=r'15 $M_{\odot}$ LMC sim')
+    ax2.loglog(freqs[good], signals[i][good], color=cmap.mpl_colors[i], lw=1)#, label=r'15 $M_{\odot}$ LMC sim')
     if i == 2:
         #plot on middle panel
-        ax2.loglog(freqs[good], signals[i][good], color=cmap.mpl_colors[i], lw=1)#, label=r'15 $M_{\odot}$ LMC sim')
+        ax1.loglog(freqs[good], signals[i][good], color=cmap.mpl_colors[i], lw=1)#, label=r'15 $M_{\odot}$ LMC sim')
 
-#con2 = ConnectionPatch(xyA=(1e1,1e-4), xyB=(4e-2,1e-4), coordsA='data', coordsB='data', axesA=ax2, axesB=ax3, color='grey', lw=0.5)
-#ax2.add_artist(con2)
-con1 = ConnectionPatch(xyA=(1e1,1.3e-1), xyB=(4e-2,1.3e-1), coordsA='data', coordsB='data', axesA=ax2, axesB=ax3, color='grey', lw=1)
-ax2.add_artist(con1)
-ax2.plot([7e0,1e1],[1.3e-1,1.3e-1], c='grey', lw=1)
+#con2 = ConnectionPatch(xyA=(1e1,1e-4), xyB=(4e-2,1e-4), coordsA='data', coordsB='data', axesA=ax1, axesB=ax2, color='grey', lw=0.5)
+#ax1.add_artist(con2)
+con1 = ConnectionPatch(xyA=(1e1,1.3e-1), xyB=(4e-2,1.3e-1), coordsA='data', coordsB='data', axesA=ax1, axesB=ax2, color='grey', lw=1)
+ax1.add_artist(con1)
+ax1.plot([7e0,1e1],[1.3e-1,1.3e-1], c='grey', lw=1)
 
-ax3.text(6.5e-2, 6e-2, r'40 $M_{\odot}$', color=cmap.mpl_colors[1], ha='center', va='center', size=8)
-ax3.text(9e-2, 5.5e-3, r'15 $M_{\odot}$', color=cmap.mpl_colors[2], ha='center', va='center', size=8)
-ax3.text(1.4e-1, 2.8e-4, r'3 $M_{\odot}$', color=cmap.mpl_colors[0], ha='center', va='center', size=8)
-ax3.set_ylim(1e-4, 1.3e-1)
-ax3.set_xlabel(r'frequency (d$^{-1}$)')
-for ax in [ax2, ax3]:
+ax2.text(6.5e-2, 6e-2, r'40 $M_{\odot}$', color=cmap.mpl_colors[1], ha='center', va='center', size=8)
+ax2.text(9e-2, 5.5e-3, r'15 $M_{\odot}$', color=cmap.mpl_colors[2], ha='center', va='center', size=8)
+ax2.text(1.4e-1, 2.8e-4, r'3 $M_{\odot}$', color=cmap.mpl_colors[0], ha='center', va='center', size=8)
+ax2.set_ylim(1e-4, 1.3e-1)
+ax2.set_xlabel(r'frequency (d$^{-1}$)')
+for ax in [ax1, ax2]:
     ax.set_xlim(4e-2, 1e1)
 
 plt.savefig('obs_prediction.png', bbox_inches='tight', dpi=300)
