59bbaa743fad437a504ba5d02b49c0f11bfe9a8f
samanthacwu <swu@carnegiescience.edu>
2025-01-13 16:01:20 -0800

    minor change

--- compstar/dedalus/star_builder.py
+++ compstar/dedalus/star_builder.py
@@ -356,7 +356,7 @@ def build_nccs(plot_nccs=False, grad_s_transition_default=0.03, reapply_grad_s_f
     # opacity_info_arr = [gff_out, z_frac_out, x_frac_out,ye_out]
     opacity_func_in = lambda rho, T: opacity_func(rho*rho_nd,T*T_nd,gff=gff_out,z_frac=z_frac_out,x_frac=x_frac_out,ye=ye_out)
     #assuming rho, T are nondimensionalized inputs, but opacity_func returns a dimensionalized opacity 
-    rad_diff_func_in = lambda rho,T: (16 * constants.sigma_sb.cgs * (T*T_nd)**3 / (3 * (rho*rho_nd)**2 * cp * opacity_func_in(rho,T))) * (tau_nd / L_nd**2)
+    # rad_diff_func_in = lambda rho,T: (16 * constants.sigma_sb.cgs * (T*T_nd)**3 / (3 * (rho*rho_nd)**2 * cp * opacity_func_in(rho,T))) * (tau_nd / L_nd**2)
     #assuming rho, T are nondimensionalized inputs, but will return dimensionalized rad_diff so must non-dimensionalize
     # will have to call HSE_solve_CZ and HSE_solve_RZ instead
     atmo = HSE_solve(c, d, bases, g_phi_func, grad_ln_rho_func, ln_rho_func, N2_func, F_conv_func,
